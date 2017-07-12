5886278ab933c1ee0ceae9819420841eedb9f669
Chad Goymer <chad.goymer@gmail.com>
2017-07-12 11:26:13 +0100

    Cleaned up the onLoad function

--- R/on-load.R
+++ R/on-load.R
@@ -1,19 +1,19 @@
 .onLoad <- function(libname, pkgname) {
-  url <- Sys.getenv("GITHUB_URL")
-  if (identical(url, ""))
-    url <- "https://github.com"
-
-  api <- Sys.getenv("GITHUB_API")
-  if (identical(api, ""))
-    api <- "https://api.github.com"
-
   token <- Sys.getenv("GITHUB_TOKEN")
   if (identical(token, ""))
     token <- Sys.getenv("GITHUB_PAT")
   if (identical(token, ""))
     message("Cannot find a GitHub token. Please set the environment variable \"GITHUB_TOKEN\".")
 
-  options(
-    github.url = url,
-    github.api = api)
+  github_env <- list(
+    GITHUB_URL = "https://github.com",
+    GITHUB_API = "https://api.github.com",
+    GITHUB_TOKEN = token)
+
+  toset <- sapply(names(github_env), function(e) identical(Sys.getenv(e), ""))
+  if (any(toset)) do.call(Sys.setenv, github_env[toset])
+
+  github_options <- as.list(Sys.getenv(names(github_env)))
+  names(github_options) <- tolower(sub("_", "\\.", names(github_env)))
+  do.call(options, github_options)
 }
