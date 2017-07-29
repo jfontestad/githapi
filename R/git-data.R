2c72f76146dc8a2d7ba2112884efe6723446a283
Chad Goymer <chad.goymer@gmail.com>
2017-07-29 08:56:28 +0100

    added gh_git_reference

--- R/git-data.R
+++ R/git-data.R
@@ -1,4 +1,4 @@
-#  FUNCTION: gh_blob --------------------------------------------------------------------------
+#  FUNCTION: gh_git_blob ----------------------------------------------------------------------
 #' Get a blob
 #'
 #' url{https://developer.github.com/v3/git/blobs/#get-a-blob}
@@ -12,7 +12,7 @@
 #' @param ... Parameters passed to \code{\link{gh_get}}.
 #' @return A string containing the contents of the specified file.
 #' @export
-gh_blob <- function(
+gh_git_blob <- function(
   sha,
   repo,
   token = gh_token(),
@@ -55,5 +55,36 @@ gh_git_commit <- function(
   assert_that(is.string(api))
 
   gh_url("repos", repo, "git/commits", sha, api = api) %>%
+    gh_page(token = token, ...)
+}
+
+#  FUNCTION: gh_git_reference -----------------------------------------------------------------
+#' Get a reference
+#'
+#' url{https://developer.github.com/v3/git/refs/#get-a-reference}
+#'
+#' @param ref (string) tags, specified as \code{tags/<tag_name>}, or branches, specified as
+#'   \code{heads/<branch_name>}
+#' @param repo (string) The repository specified in the format: \code{"owner/repo"}.
+#' @param token (string, optional) The personal access token for GitHub authorisation. Default:
+#'   value stored in the environment variable \code{"GITHUB_TOKEN"} or \code{"GITHUB_PAT"}.
+#' @param api (string, optional) The URL of GitHub's API. Default: the value stored in the
+#'   environment variable \code{"GITHUB_API_URL"} or \code{"https://api.github.com"}.
+#' @param ... Parameters passed to \code{\link{gh}}.
+#' @return A list describing the reference (see GitHub's API documentation for details).
+#' @export
+gh_git_reference <- function(
+  ref,
+  repo,
+  token = gh_token(),
+  api   = getOption("github.api"),
+  ...)
+{
+  assert_that(is.string(ref))
+  assert_that(is.string(repo) && identical(str_count(repo, "/"), 1L))
+  assert_that(is.string(token) && identical(str_length(token), 40L))
+  assert_that(is.string(api))
+
+  gh_url("repos", repo, "git/refs", ref, api = api) %>%
     gh_page(token = token, ...)
 }
--- tests/testthat/test-git-data.R
+++ tests/testthat/test-git-data.R
@@ -1,8 +1,8 @@
 context("git data api")
 
-#  FUNCTION: gh_blob --------------------------------------------------------------------------
-test_that("gh_blob returns the contents of a file in the repository", {
-  readme <- gh_blob("abb7f8ce52e6bdea33170ec8edbd6cfb1eca0722", "ChadGoymer/githapi")
+#  FUNCTION: gh_git_blob ----------------------------------------------------------------------
+test_that("gh_git_blob returns the contents of a file in the repository", {
+  readme <- gh_git_blob("abb7f8ce52e6bdea33170ec8edbd6cfb1eca0722", "ChadGoymer/githapi")
   expect_true(is.string(readme))
   expect_identical(
     readme,
@@ -19,4 +19,15 @@ test_that("gh_git_commit returns a list of information about a commit", {
   expect_identical(git_commit$sha, "7ca61bb71f877f462c0b6132759d7c5e507c921f")
   expect_identical(git_commit$author$name, "Chad Goymer")
   expect_identical(git_commit$message, "removed reference to github_url\n\nAlso added gh_readme and gh_commit_sha")
+})
+
+#  FUNCTION: gh_git_reference -----------------------------------------------------------------
+test_that("gh_git_reference returns information about a git reference", {
+  head_master <- gh_git_reference("heads/master", "ChadGoymer/githapi")
+  expect_is(head_master, "list")
+  expect_identical(head_master$ref, "refs/heads/master")
+
+  tag_v0.1.0 <- gh_git_reference("tags/v0.1.0", "ChadGoymer/githapi")
+  expect_is(tag_v0.1.0, "list")
+  expect_identical(tag_v0.1.0$ref, "refs/tags/v0.1.0")
 })
