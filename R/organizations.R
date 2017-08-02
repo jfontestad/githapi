f09b65c6e506d90ad6566feab2ceb5193c9aeb67
Chad Goymer <chad.goymer@gmail.com>
2017-08-02 16:01:54 +0100

    added gh_organization

--- R/organizations.R
+++ R/organizations.R
@@ -1,3 +1,30 @@
+#  FUNCTION: gh_organization ------------------------------------------------------------------
+#' Get an organization
+#'
+#' url{https://developer.github.com/v3/orgs/#get-an-organization}
+#'
+#' @param org (string) The name of the organization.
+#' @param token (string, optional) The personal access token for GitHub authorisation. Default:
+#'   value stored in the environment variable \code{"GITHUB_TOKEN"} or \code{"GITHUB_PAT"}.
+#' @param api (string, optional) The URL of GitHub's API. Default: the value stored in the
+#'   environment variable \code{"GITHUB_API_URL"} or \code{"https://api.github.com"}.
+#' @param ... Parameters passed to \code{\link{gh_page}}.
+#' @return A list describing the organization (see GitHub's API documentation for details).
+#' @export
+gh_organization <- function(
+  org,
+  token = gh_token(),
+  api   = getOption("github.api"),
+  ...)
+{
+  assert_that(is.string(org))
+  assert_that(is.string(token) && identical(str_length(token), 40L))
+  assert_that(is.string(api))
+
+  gh_url("orgs", org, api = api) %>%
+    gh_page(token = token, ...)
+}
+
 #  FUNCTION: gh_organizations -----------------------------------------------------------------
 #' List organizations
 #'
--- tests/testthat/test-organizations.R
+++ tests/testthat/test-organizations.R
@@ -1,5 +1,19 @@
 context("organizations api")
 
+#  FUNCTION: gh_organization ------------------------------------------------------------------
+test_that("gh_organization returns a list describing the organization", {
+  tidyverse <- gh_organization("tidyverse")
+  expect_is(tidyverse, "list")
+  expect_identical(
+    names(tidyverse),
+    c("login", "id", "url", "repos_url", "events_url", "hooks_url", "issues_url",
+      "members_url", "public_members_url", "avatar_url", "description", "name", "company",
+      "blog", "location", "email", "has_organization_projects", "has_repository_projects",
+      "public_repos", "public_gists", "followers", "following", "html_url", "created_at",
+      "updated_at", "type"))
+  expect_identical(tidyverse$login, "tidyverse")
+})
+
 #  FUNCTION: gh_organizations -----------------------------------------------------------------
 test_that("gh_organizations returns a tibble describing the organizations", {
   hadley_orgs <- gh_organizations("hadley")
