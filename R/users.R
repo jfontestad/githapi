6121f61a8940b1ad3eb18e40b192b4120001ed40
ChadGoymer <chad.goymer@gmail.com>
2017-08-14 17:48:58 +0100

    added gh_user_email

--- R/users.R
+++ R/users.R
@@ -37,7 +37,6 @@ gh_user <- function(
 #'
 #' \url{https://developer.github.com/v3/users/#get-all-users}
 #'
-#' @param limit (integer, optional) The maximum number to return. Default: 1000.
 #' @param token (string, optional) The personal access token for GitHub authorisation. Default:
 #'   string stored in the environment variable \code{GITHUB_TOKEN} or \code{GITHUB_PAT}.
 #' @param api (string, optional) The URL of GitHub's API. Default: the value stored in the
@@ -58,4 +57,32 @@ gh_users <- function(
     .[] %>%
     bind_rows %>%
     select(login, type, html_url, url)
+}
+
+#  FUNCTION: gh_user_email --------------------------------------------------------------------
+#' List email addresses for the authenticated user
+#'
+#' url{https://developer.github.com/v3/users/emails/#list-email-addresses-for-a-user}
+#' url{https://developer.github.com/v3/users/emails/#list-public-email-addresses-for-a-user}
+#'
+#' @param token (string, optional) The personal access token for GitHub authorisation. Default:
+#'   string stored in the environment variable \code{GITHUB_TOKEN} or \code{GITHUB_PAT}.
+#' @param api (string, optional) The URL of GitHub's API. Default: the value stored in the
+#'   environment variable \code{GITHUB_API_URL} or \code{https://api.github.com}.
+#' @param ... Parameters passed to \code{\link{gh_page}}.
+#' @return A tibble describing the user's email addresses (see GitHub's API documentation for
+#'   more detail).
+#' @export
+gh_user_email <- function(
+  token = gh_token(),
+  api   = getOption("github.api"),
+  ...)
+{
+  assert_that(is.string(token) && identical(str_length(token), 40L))
+  assert_that(is.string(api))
+
+  gh_url("user/emails", api = api) %>%
+    gh_page(token = token, ...) %>%
+    map(flatten_) %>%
+    bind_rows()
 }
