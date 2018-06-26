a86526b93e6f7ea961704b8f1c7d6e68d3f9eedd
Unknown <chad.goymer@lloyds.com>
2018-06-26 20:57:06 +0100

    updated gists API

--- R/gists.R
+++ R/gists.R
@@ -1,4 +1,5 @@
 #  FUNCTION: gh_gist --------------------------------------------------------------------------
+#
 #' Get a single gist
 #'
 #' url{https://developer.github.com/v3/gists/#get-a-single-gist}
@@ -11,8 +12,11 @@
 #' @param api (string, optional) The URL of GitHub's API. Default: the value stored in the
 #'   environment variable \code{"GITHUB_API_URL"} or \code{"https://api.github.com"}.
 #' @param ... Parameters passed to \code{\link{gh_get}}.
+#'
 #' @return A list describing the gist (see GitHub's API documentation for details).
+#'
 #' @export
+#'
 gh_gist <- function(
   gist,
   sha,
@@ -20,21 +24,22 @@ gh_gist <- function(
   api   = getOption("github.api"),
   ...)
 {
-  assert_that(is.string(gist))
-  assert_that(is.string(token) && identical(str_length(token), 40L))
-  assert_that(is.string(api))
+  stopifnot(is_string(gist))
+  stopifnot(is_sha(token))
+  stopifnot(is_url(api))
 
   if (missing(sha)) {
     url <- gh_url("gists", gist, api = api)
   } else {
-    assert_that(is.string(sha) && identical(str_length(sha), 40L))
+    stopifnot(is_sha(sha))
     url <- gh_url("gists", gist, sha, api = api)
   }
 
-  url %>% gh_get(token = token, ...)
+  gh_get(url, token = token, ...)
 }
 
 #  FUNCTION: gh_gists -------------------------------------------------------------------------
+#
 #' List a user's gists, or all public or starred gists.
 #'
 #' url{https://developer.github.com/v3/gists/#list-a-users-gists}
@@ -53,8 +58,11 @@ gh_gist <- function(
 #' @param api (string, optional) The URL of GitHub's API. Default: the value stored in the
 #'   environment variable \code{"GITHUB_API_URL"} or \code{"https://api.github.com"}.
 #' @param ... Parameters passed to \code{\link{gh_page}}.
+#'
 #' @return A tibble describing the gists (see GitHub's API documentation for details).
+#'
 #' @export
+#'
 gh_gists <- function(
   user,
   since = NULL,
@@ -63,41 +71,44 @@ gh_gists <- function(
   api   = getOption("github.api"),
   ...)
 {
-  assert_that(is.null(since) || is.string(since))
-  assert_that(is.count(n_max))
-  assert_that(is.string(token) && identical(str_length(token), 40L))
-  assert_that(is.string(api))
+  stopifnot(is.null(since) || is_string(since))
+  stopifnot(is_count(n_max))
+  stopifnot(is_sha(token))
+  stopifnot(is_url(api))
 
   if (missing(user)) {
-    url <- gh_url("gists", api = api)
+    url <- gh_url("gists", since = since, api = api)
   } else {
-    assert_that(is.string(user))
+    stopifnot(is_string(user))
     if (user %in% c("public", "starred")) {
-      url <- gh_url("gists", user)
+      url <- gh_url("gists", user, since = since, api = api)
     } else {
-      url <- gh_url("users", user, "gists")
+      url <- gh_url("users", user, "gists", since = since, api = api)
     }
   }
 
-  url %>%
-    gh_page(n_max = n_max, token = token, ...) %>%
-    map(function(g) {g$files <- unname(g$files); g}) %>%
-    toJSON(auto_unbox = TRUE) %>%
-    fromJSON(simplifyDataFrame = TRUE, flatten = TRUE) %>%
-    as_tibble() %>%
-    set_names(str_replace_all(names(.), "\\.", "_")) %>%
-    mutate(
-      description = ifelse(as.character(description) == "list()", NA, as.character(description)),
-      filenames   = collapse_list(files, "filename"),
-      languages   = collapse_list(files, "language"),
-      file_sizes  = collapse_list(files, "size")) %>%
-    select_safe(
-      id, description, owner_login, created_at, updated_at, comments,
-      filenames, languages, file_sizes, public, url) %>%
-    mutate(created_at  = parse_datetime(created_at), updated_at  = parse_datetime(updated_at))
+  gists <- gh_page(url, n_max = n_max, token = token, ...)
+
+  bind_fields(gists, list(
+    id          = c("id",             as = "character"),
+    description = c("description",    as = "character"),
+    owner_login = c("owner", "login", as = "character"),
+    created_at  = c("created_at",     as = "datetime"),
+    updated_at  = c("updated_at",     as = "datetime"),
+    comments    = c("comments",       as = "integer"),
+    files       = "",
+    public      = c("public",         as = "logical"),
+    url         = c("url",            as = "character"))) %>%
+    mutate(files = lapply(gists, function(g) {
+      bind_fields(g$files, list(
+        filename = c("filename", as = "character"),
+        language = c("language", as = "character"),
+        size     = c("size",     as = "integer")))
+    }))
 }
 
 #  FUNCTION: gh_gist_commits ------------------------------------------------------------------
+#
 #' List gist commits
 #'
 #' url{https://developer.github.com/v3/gists/#list-gist-commits}
@@ -109,8 +120,11 @@ gh_gists <- function(
 #' @param api (string, optional) The URL of GitHub's API. Default: the value stored in the
 #'   environment variable \code{"GITHUB_API_URL"} or \code{"https://api.github.com"}.
 #' @param ... Parameters passed to \code{\link{gh_get}}.
+#'
 #' @return A tibble describing the gist commits (see GitHub's API documentation for details).
+#'
 #' @export
+#'
 gh_gist_commits <- function(
   gist,
   n_max = 1000L,
@@ -118,20 +132,27 @@ gh_gist_commits <- function(
   api   = getOption("github.api"),
   ...)
 {
-  assert_that(is.string(gist))
-  assert_that(is.count(n_max))
-  assert_that(is.string(token) && identical(str_length(token), 40L))
-  assert_that(is.string(api))
+  stopifnot(is_string(gist))
+  stopifnot(is_count(n_max))
+  stopifnot(is_sha(token))
+  stopifnot(is_url(api))
 
-  gh_url("gists", gist, "commits", api = api) %>%
-    gh_page(simplify = TRUE, n_max = n_max, token = token, ...) %>%
-    select_safe(
-      version, user_login, committed_at, change_status_total,
-      change_status_additions, change_status_deletions, url) %>%
-    mutate(committed_at = parse_datetime(committed_at))
+  commits <- gh_page(
+    gh_url("gists", gist, "commits", api = api),
+    n_max = n_max, token = token, ...)
+
+  bind_fields(commits, list(
+    version           = c("version",                    as = "character"),
+    user_login        = c("user", "login",              as = "character"),
+    committed_at      = c("committed_at",               as = "datetime"),
+    changes_total     = c("change_status", "total",     as = "integer"),
+    changes_additions = c("change_status", "additions", as = "integer"),
+    changes_deletions = c("change_status", "deletions", as = "integer"),
+    url               = c("url",                        as = "character")))
 }
 
 #  FUNCTION: is_gist_starred ------------------------------------------------------------------
+#
 #' Check if a gist is starred
 #'
 #' url{https://developer.github.com/v3/gists/#check-if-a-gist-is-starred}
@@ -142,24 +163,29 @@ gh_gist_commits <- function(
 #' @param api (string, optional) The URL of GitHub's API. Default: the value stored in the
 #'   environment variable \code{"GITHUB_API_URL"} or \code{"https://api.github.com"}.
 #' @param ... Parameters passed to \code{\link{gh_get}}.
+#'
 #' @return TRUE if the gist has been starred, FALSE otherwise (see GitHub's API documentation
 #'   for details).
+#'
 #' @export
+#'
 is_gist_starred <- function(
   gist,
   token = gh_token(),
   api   = getOption("github.api"),
   ...)
 {
-  assert_that(is.string(gist))
-  assert_that(is.string(token) && identical(str_length(token), 40L))
-  assert_that(is.string(api))
+  stopifnot(is_string(gist))
+  stopifnot(is_sha(token))
+  stopifnot(is_url(api))
 
   response <- try(silent = TRUE, suppressMessages({
-    gh_url("gists", gist, "star", api = api) %>%
-      gh_get(accept = "raw", token = token, ...)
+    gh_get(
+      gh_url("gists", gist, "star", api = api),
+      accept = "raw", token = token, ...)
   }))
 
+  attributes(response) <- NULL
   if (identical(response, "")) {
     TRUE
   } else {
@@ -168,6 +194,7 @@ is_gist_starred <- function(
 }
 
 #  FUNCTION: gh_gist_forks --------------------------------------------------------------------
+#
 #' List gist forks
 #'
 #' url{https://developer.github.com/v3/gists/#list-gist-forks}
@@ -179,8 +206,11 @@ is_gist_starred <- function(
 #' @param api (string, optional) The URL of GitHub's API. Default: the value stored in the
 #'   environment variable \code{"GITHUB_API_URL"} or \code{"https://api.github.com"}.
 #' @param ... Parameters passed to \code{\link{gh_get}}.
+#'
 #' @return A tibble describing the forks (see GitHub's API documentation for details).
+#'
 #' @export
+#'
 gh_gist_forks <- function(
   gist,
   n_max = 1000L,
@@ -188,18 +218,28 @@ gh_gist_forks <- function(
   api   = getOption("github.api"),
   ...)
 {
-  assert_that(is.string(gist))
-  assert_that(is.count(n_max))
-  assert_that(is.string(token) && identical(str_length(token), 40L))
-  assert_that(is.string(api))
+  stopifnot(is_string(gist))
+  stopifnot(is_count(n_max))
+  stopifnot(is_sha(token))
+  stopifnot(is_url(api))
 
-  gh_url("gists", gist, "forks", api = api) %>%
-    gh_page(simplify = TRUE, n_max = n_max, token = token, ...) %>%
-    select_safe(id, description, owner_login, created_at, updated_at, public, comments, url) %>%
-    mutate(created_at = parse_datetime(created_at), updated_at = parse_datetime(updated_at))
+  forks <- gh_page(
+    gh_url("gists", gist, "forks", api = api),
+    n_max = n_max, token = token, ...)
+
+  bind_fields(forks, list(
+    id          = c("id",             as = "character"),
+    description = c("description",    as = "character"),
+    owner_login = c("owner", "login", as = "character"),
+    created_at  = c("created_at",     as = "datetime"),
+    updated_at  = c("updated_at",     as = "datetime"),
+    public      = c("public",         as = "logical"),
+    comments    = c("comments",       as = "integer"),
+    url         = c("url",            as = "character")))
 }
 
 #  FUNCTION: gh_gist_comment ------------------------------------------------------------------
+#
 #' Get a single comment
 #'
 #' url{https://developer.github.com/v3/gists/comments/#get-a-single-comment}
@@ -211,8 +251,11 @@ gh_gist_forks <- function(
 #' @param api (string, optional) The URL of GitHub's API. Default: the value stored in the
 #'   environment variable \code{"GITHUB_API_URL"} or \code{"https://api.github.com"}.
 #' @param ... Parameters passed to \code{\link{gh_get}}.
+#'
 #' @return A list describing the gist comment (see GitHub's API documentation for details).
+#'
 #' @export
+#'
 gh_gist_comment <- function(
   comment,
   gist,
@@ -220,16 +263,18 @@ gh_gist_comment <- function(
   api   = getOption("github.api"),
   ...)
 {
-  assert_that(is.count(comment))
-  assert_that(is.string(gist))
-  assert_that(is.string(token) && identical(str_length(token), 40L))
-  assert_that(is.string(api))
+  stopifnot(is_count(comment))
+  stopifnot(is_string(gist))
+  stopifnot(is_sha(token))
+  stopifnot(is_url(api))
 
-  gh_url("gists", gist, "comments", comment, api = api) %>%
-    gh_get(token = token, ...)
+  gh_get(
+    gh_url("gists", gist, "comments", comment, api = api),
+    token = token, ...)
 }
 
 #  FUNCTION: gh_gist_comments -----------------------------------------------------------------
+#
 #' List comments on a gist
 #'
 #' url{https://developer.github.com/v3/gists/comments/#list-comments-on-a-gist}
@@ -241,8 +286,11 @@ gh_gist_comment <- function(
 #' @param api (string, optional) The URL of GitHub's API. Default: the value stored in the
 #'   environment variable \code{"GITHUB_API_URL"} or \code{"https://api.github.com"}.
 #' @param ... Parameters passed to \code{\link{gh_get}}.
+#'
 #' @return A tibble describing the gist comments (see GitHub's API documentation for details).
+#'
 #' @export
+#'
 gh_gist_comments <- function(
   gist,
   n_max = 1000L,
@@ -250,18 +298,26 @@ gh_gist_comments <- function(
   api   = getOption("github.api"),
   ...)
 {
-  assert_that(is.string(gist))
-  assert_that(is.count(n_max))
-  assert_that(is.string(token) && identical(str_length(token), 40L))
-  assert_that(is.string(api))
+  stopifnot(is_string(gist))
+  stopifnot(is_count(n_max))
+  stopifnot(is_sha(token))
+  stopifnot(is_url(api))
 
-  gh_url("gists", gist, "comments", api = api) %>%
-    gh_page(simplify = TRUE, n_max = n_max, token = token, ...) %>%
-    select_safe(id, body, user_login, created_at, updated_at, url) %>%
-    mutate(created_at = parse_datetime(created_at), updated_at = parse_datetime(updated_at))
+  comments <- gh_page(
+    gh_url("gists", gist, "comments", api = api),
+    n_max = n_max, token = token, ...)
+
+  bind_fields(comments, list(
+    id         = c("id",            as = "integer"),
+    body       = c("body",          as = "character"),
+    user_login = c("user", "login", as = "character"),
+    created_at = c("created_at",    as = "datetime"),
+    updated_at = c("updated_at",    as = "datetime"),
+    url        = c("url",           as = "character")))
 }
 
 #  FUNCTION: gh_save_gist -------------------------------------------------------------------------
+#
 #' Save files in a gist
 #'
 #' url{https://developer.github.com/v3/gists/#get-a-single-gist}
@@ -277,44 +333,46 @@ gh_gist_comments <- function(
 #' @param api (string, optional) The URL of GitHub's API. Default: the value stored in the
 #'   environment variable \code{"GITHUB_API_URL"} or \code{"https://api.github.com"}.
 #' @param ... Parameters passed to \code{\link{gh_get}}.
+#'
 #' @return The file path of the saved file (invisibly).
+#'
 #' @export
+#'
 gh_save_gist <- function(
   gist,
   path,
   files,
   sha,
-  token   = gh_token(),
-  api     = getOption("github.api"),
+  token = gh_token(),
+  api   = getOption("github.api"),
   ...)
 {
-  assert_that(is.string(gist))
-  assert_that(is.string(path))
-  assert_that(is.string(token) && identical(str_length(token), 40L))
-  assert_that(is.string(api))
+  stopifnot(is_string(gist))
+  stopifnot(is_string(path))
+  stopifnot(is_sha(token))
+  stopifnot(is_url(api))
 
-  gist_files <- gh_gist(gist, sha = sha, token = token, api = api) %>%
-    .[["files"]] %>%
-    bind_rows()
+  gist_files <- gh_gist(gist, sha = sha, token = token, api = api)[["files"]]
+  gist_urls <- sapply(gist_files, getElement, "raw_url")
 
   if (!missing(files)) {
-    assert_that(is.character(files))
-    if (!all(files %in% gist_files$filename))
+    stopifnot(is.character(files))
+    if (!all(files %in% names(gist_urls)))
       stop("Cannot find all specified files")
-    gist_files <- filter(gist_files, filename %in% files)
+    gist_urls <- gist_urls[names(gist_urls) %in% files]
   }
 
   if (!dir.exists(path)) dir.create(path)
 
-  map2(gist_files$filename, gist_files$raw_url, function(filename, url) {
-    gh_get(url, accept = "raw", binary = TRUE) %>%
-      writeBin(file.path(path, basename(filename)))
-  })
+  for (gist in names(gist_urls)) {
+    gh_download_binary(gist_urls[[gist]], path = file.path(path, basename(gist)), token = token)
+  }
 
   invisible(path)
 }
 
 #  FUNCTION: gh_source_gist -------------------------------------------------------------------
+#
 #' Source an R file from a gist
 #'
 #' url{https://developer.github.com/v3/gists/#get-a-single-gist}
@@ -328,8 +386,11 @@ gh_save_gist <- function(
 #' @param api (string, optional) The URL of GitHub's API. Default: the value stored in the
 #'   environment variable \code{"GITHUB_API_URL"} or \code{"https://api.github.com"}.
 #' @param ... Parameters passed to \code{\link{source}}.
+#'
 #' @return Nothing. The file is sourced into global environment.
+#'
 #' @export
+#'
 gh_source_gist <- function(
   file,
   gist,
@@ -338,10 +399,10 @@ gh_source_gist <- function(
   api   = getOption("github.api"),
   ...)
 {
-  assert_that(is.string(file))
-  assert_that(is.string(gist))
-  assert_that(is.string(token) && identical(str_length(token), 40L))
-  assert_that(is.string(api))
+  stopifnot(is_string(file))
+  stopifnot(is_string(gist))
+  stopifnot(is_sha(token))
+  stopifnot(is_url(api))
 
   temp_path <- tempdir()
   on.exit(unlink(file.path(tempdir(), basename(file)), recursive = TRUE))
