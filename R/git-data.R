173f78fc1b7ac0b90f50e57f2b5e8fa05513afb2
Unknown <chad.goymer@lloyds.com>
2018-06-26 20:56:41 +0100

    updated git-data API

--- R/git-data.R
+++ R/git-data.R
@@ -1,4 +1,5 @@
 #  FUNCTION: gh_git_blob ----------------------------------------------------------------------
+#
 #' Get a blob
 #'
 #' url{https://developer.github.com/v3/git/blobs/#get-a-blob}
@@ -10,9 +11,12 @@
 #' @param api (string, optional) The URL of GitHub's API. Default: the value stored in the
 #'   environment variable \code{"GITHUB_API_URL"} or \code{"https://api.github.com"}.
 #' @param ... Parameters passed to \code{\link{gh_get}}.
+#'
 #' @return A string containing the contents of the specified file (see GitHub's API
 #'   documentation for details).
+#'
 #' @export
+#'
 gh_git_blob <- function(
   sha,
   repo,
@@ -20,16 +24,21 @@ gh_git_blob <- function(
   api   = getOption("github.api"),
   ...)
 {
-  assert_that(is.string(sha) && identical(str_length(sha), 40L))
-  assert_that(is.string(repo) && identical(str_count(repo, "/"), 1L))
-  assert_that(is.string(token) && identical(str_length(token), 40L))
-  assert_that(is.string(api))
+  stopifnot(is_sha(sha))
+  stopifnot(is_repo(repo))
+  stopifnot(is_sha(token))
+  stopifnot(is_url(api))
 
-  gh_url("repos", repo, "git/blobs", sha, api = api) %>%
-    gh_get(accept = "raw", token = token, ...)
+  blob <- gh_get(
+    gh_url("repos", repo, "git/blobs", sha, api = api),
+    accept = "raw", token = token, ...)
+
+  attr(blob, "header") <- NULL
+  blob
 }
 
 #  FUNCTION: gh_git_commit --------------------------------------------------------------------
+#
 #' Get a commit
 #'
 #' url{https://developer.github.com/v3/git/commits/#get-a-commit}
@@ -41,8 +50,11 @@ gh_git_blob <- function(
 #' @param api (string, optional) The URL of GitHub's API. Default: the value stored in the
 #'   environment variable \code{"GITHUB_API_URL"} or \code{"https://api.github.com"}.
 #' @param ... Parameters passed to \code{\link{gh_get}}.
+#'
 #' @return A list describing the commit (see GitHub's API documentation for details).
+#'
 #' @export
+#'
 gh_git_commit <- function(
   sha,
   repo,
@@ -50,16 +62,18 @@ gh_git_commit <- function(
   api   = getOption("github.api"),
   ...)
 {
-  assert_that(is.string(sha) && identical(str_length(sha), 40L))
-  assert_that(is.string(repo) && identical(str_count(repo, "/"), 1L))
-  assert_that(is.string(token) && identical(str_length(token), 40L))
-  assert_that(is.string(api))
+  stopifnot(is_sha(sha))
+  stopifnot(is_repo(repo))
+  stopifnot(is_sha(token))
+  stopifnot(is_url(api))
 
-  gh_url("repos", repo, "git/commits", sha, api = api) %>%
-    gh_get(token = token, ...)
+  gh_get(
+    gh_url("repos", repo, "git/commits", sha, api = api),
+    token = token, ...)
 }
 
 #  FUNCTION: gh_git_reference -----------------------------------------------------------------
+#
 #' Get a reference
 #'
 #' url{https://developer.github.com/v3/git/refs/#get-a-reference}
@@ -72,8 +86,11 @@ gh_git_commit <- function(
 #' @param api (string, optional) The URL of GitHub's API. Default: the value stored in the
 #'   environment variable \code{"GITHUB_API_URL"} or \code{"https://api.github.com"}.
 #' @param ... Parameters passed to \code{\link{gh_get}}.
+#'
 #' @return A list describing the reference (see GitHub's API documentation for details).
+#'
 #' @export
+#'
 gh_git_reference <- function(
   ref,
   repo,
@@ -81,16 +98,18 @@ gh_git_reference <- function(
   api   = getOption("github.api"),
   ...)
 {
-  assert_that(is.string(ref))
-  assert_that(is.string(repo) && identical(str_count(repo, "/"), 1L))
-  assert_that(is.string(token) && identical(str_length(token), 40L))
-  assert_that(is.string(api))
+  stopifnot(is_string(ref))
+  stopifnot(is_repo(repo))
+  stopifnot(is_sha(token))
+  stopifnot(is_url(api))
 
-  gh_url("repos", repo, "git/refs", ref, api = api) %>%
-    gh_get(token = token, ...)
+  gh_get(
+    gh_url("repos", repo, "git/refs", ref, api = api),
+    token = token, ...)
 }
 
 #  FUNCTION: is_tag ---------------------------------------------------------------------------
+#
 #' Check whether the input is a valid tag.
 #'
 #' url{https://developer.github.com/v3/git/refs/#get-a-reference}
@@ -102,8 +121,11 @@ gh_git_reference <- function(
 #' @param api (string, optional) The URL of GitHub's API. Default: the value stored in the
 #'   environment variable \code{"GITHUB_API_URL"} or \code{"https://api.github.com"}.
 #' @param ... Parameters passed to \code{\link{gh_page}}.
+#'
 #' @return A boolean, with attributes describing the errors, if there are any.
+#'
 #' @export
+#'
 is_tag <- function(
   tag,
   repo,
@@ -111,30 +133,23 @@ is_tag <- function(
   api   = getOption("github.api"),
   ...)
 {
-  if (!is.string(tag)) {
-    return(
-      FALSE %>%
-        set_attrs(error = str_c("Specified 'tag', '", tag, "', is not a string")))
+  if (!is_string(tag)) {
+    return(FALSE)
   }
 
   result <- try(
-    gh_git_reference(ref = str_c("tags/", tag), repo = repo, token = token, api = api, ...),
+    gh_git_reference(ref = paste0("tags/", tag), repo = repo, token = token, api = api, ...),
     silent = TRUE)
 
-  if (is (result, "try-error")) {
-    if (is(attr(result, "condition"), "http_404")) {
-      return(
-        FALSE %>%
-          set_attrs(error = str_c("Specified 'tag', '", tag, "', does not exist in the 'repo' '", repo, "'")))
-    } else {
-      stop(result)
-    }
+  if (is(result, "try-error")) {
+    return(FALSE)
   }
 
   TRUE
 }
 
 #  FUNCTION: gh_git_references ----------------------------------------------------------------
+#
 #' Get all references
 #'
 #' url{https://developer.github.com/v3/git/refs/#get-all-references}
@@ -146,8 +161,11 @@ is_tag <- function(
 #' @param api (string, optional) The URL of GitHub's API. Default: the value stored in the
 #'   environment variable \code{"GITHUB_API_URL"} or \code{"https://api.github.com"}.
 #' @param ... Parameters passed to \code{\link{gh_page}}.
+#'
 #' @return A tibble describing the references (see GitHub's API documentation for details).
+#'
 #' @export
+#'
 gh_git_references <- function(
   repo,
   n_max = 1000L,
@@ -155,10 +173,10 @@ gh_git_references <- function(
   api   = getOption("github.api"),
   ...)
 {
-  assert_that(is.string(repo) && identical(str_count(repo, "/"), 1L))
-  assert_that(is.count(n_max))
-  assert_that(is.string(token) && identical(str_length(token), 40L))
-  assert_that(is.string(api))
+  stopifnot(is_repo(repo))
+  stopifnot(is_count(n_max))
+  stopifnot(is_sha(token))
+  stopifnot(is_url(api))
 
   ref_map <- c(
     "refs/heads" = "branch",
@@ -166,15 +184,24 @@ gh_git_references <- function(
     "head"       = "pull request",
     "merge"      = "pull merge")
 
-  gh_url("repos", repo, "git/refs", api = api) %>%
-    gh_page(simplify = TRUE, n_max = n_max, token = token, ...) %>%
-    select_safe(name, type, object_type, object_sha, ref, url) %>%
+  references <- gh_page(
+    gh_url("repos", repo, "git/refs", api = api),
+    n_max = n_max, token = token, ...)
+
+  bind_fields(references, list(
+    name        = "",
+    type        = "",
+    object_type = c("object", "type", as = "character"),
+    object_sha  = c("object", "sha",  as = "character"),
+    ref         = c("ref",            as = "character"),
+    url         = c("url",            as = "character"))) %>%
     mutate(
-      name = ifelse(str_detect(ref, "pull"), basename(dirname(ref)), basename(ref)),
-      type = ifelse(str_detect(ref, "pull"), ref_map[basename(ref)], ref_map[dirname(ref)]))
+      name = ifelse(grepl("pull", ref), basename(dirname(ref)), basename(ref)),
+      type = ifelse(grepl("pull", ref), ref_map[basename(ref)], ref_map[dirname(ref)]))
 }
 
 #  FUNCTION: gh_git_tag -----------------------------------------------------------------------
+#
 #' Get an annotated tag
 #'
 #' url{https://developer.github.com/v3/git/tags/#get-a-tag}
@@ -186,8 +213,11 @@ gh_git_references <- function(
 #' @param api (string, optional) The URL of GitHub's API. Default: the value stored in the
 #'   environment variable \code{"GITHUB_API_URL"} or \code{"https://api.github.com"}.
 #' @param ... Parameters passed to \code{\link{gh_get}}.
+#'
 #' @return A list describing the tag (see GitHub's API documentation for details).
+#'
 #' @export
+#'
 gh_git_tag <- function(
   sha,
   repo,
@@ -195,16 +225,18 @@ gh_git_tag <- function(
   api   = getOption("github.api"),
   ...)
 {
-  assert_that(is.string(sha) && identical(str_length(sha), 40L))
-  assert_that(is.string(repo) && identical(str_count(repo, "/"), 1L))
-  assert_that(is.string(token) && identical(str_length(token), 40L))
-  assert_that(is.string(api))
+  stopifnot(is_sha(sha))
+  stopifnot(is_repo(repo))
+  stopifnot(is_sha(token))
+  stopifnot(is_url(api))
 
-  gh_url("repos", repo, "git/tags", sha, api = api) %>%
-    gh_get(token = token, ...)
+  gh_get(
+    gh_url("repos", repo, "git/tags", sha, api = api),
+    token = token, ...)
 }
 
 #  FUNCTION: gh_git_tree ----------------------------------------------------------------------
+#
 #' Get a tree
 #'
 #' url{https://developer.github.com/v3/git/trees/#get-a-tree}
@@ -217,9 +249,12 @@ gh_git_tag <- function(
 #' @param api (string, optional) The URL of GitHub's API. Default: the value stored in the
 #'   environment variable \code{"GITHUB_API_URL"} or \code{"https://api.github.com"}.
 #' @param ... Parameters passed to \code{\link{gh_get}}.
+#'
 #' @return A tibble describing the files in a commit (see GitHub's API documentation for
 #'   details).
+#'
 #' @export
+#'
 gh_git_tree <- function(
   ref,
   repo,
@@ -228,17 +263,26 @@ gh_git_tree <- function(
   api       = getOption("github.api"),
   ...)
 {
-  assert_that(is.string(ref))
-  assert_that(is.string(repo) && identical(str_count(repo, "/"), 1L))
-  assert_that(is.string(token) && identical(str_length(token), 40L))
-  assert_that(is.string(api))
+  stopifnot(is_string(ref))
+  stopifnot(is_repo(repo))
+  stopifnot(is_flag(recursive))
+  stopifnot(is_sha(token))
+  stopifnot(is_url(api))
 
-  gh_url("repos", repo, "git/trees", ref, recursive = as.integer(recursive), api = api) %>%
-    gh_get(sub_list = "tree", simplify = TRUE, token = token, ...) %>%
-    select_safe(path, type, sha, size, url)
+  tree <- gh_get(
+    gh_url("repos", repo, "git/trees", ref, recursive = as.integer(recursive), api = api),
+    token = token, ...)
+
+  bind_fields(tree$tree, list(
+    path = c("path", as = "character"),
+    type = c("type", as = "character"),
+    sha  = c("sha",  as = "character"),
+    size = c("size", as = "integer"),
+    url  = c("url",  as = "character")))
 }
 
 #  FUNCTION: gh_save --------------------------------------------------------------------------
+#
 #' Download files and save them to a location
 #'
 #' url{https://developer.github.com/v3/git/blobs/#get-a-blob}
@@ -253,8 +297,11 @@ gh_git_tree <- function(
 #' @param api (string, optional) The URL of GitHub's API. Default: the value stored in the
 #'   environment variable \code{"GITHUB_API_URL"} or \code{"https://api.github.com"}.
 #' @param ... Parameters passed to \code{\link{gh_get}}.
+#'
 #' @return The file path of the saved file (invisibly).
+#'
 #' @export
+#'
 gh_save <- function(
   files,
   repo,
@@ -264,34 +311,37 @@ gh_save <- function(
   api   = getOption("github.api"),
   ...)
 {
-  assert_that(is.character(files))
-  assert_that(is.string(repo) && identical(str_count(repo, "/"), 1L))
-  assert_that(is.string(path))
-  assert_that(is.string(ref))
-  assert_that(is.string(token) && identical(str_length(token), 40L))
-  assert_that(is.string(api))
+  stopifnot(is.character(files))
+  stopifnot(is_repo(repo))
+  stopifnot(is_string(path))
+  stopifnot(is_string(ref))
+  stopifnot(is_sha(token))
+  stopifnot(is_url(api))
 
   repo_files <- gh_git_tree(ref = ref, repo = repo, recursive = TRUE, token = token, api = api)
 
   if (all(files %in% repo_files$path)) {
-    blob_shas <- repo_files %>% filter(path %in% files) %>% select_safe(path, sha)
+    blob_paths <- repo_files[repo_files$path %in% files,][["path"]]
+    names(blob_paths) <- repo_files[repo_files$path %in% files,][["sha"]]
   } else {
     stop(
       "Cannot find specified files in repo '", repo, "':\n\n  ",
-      str_c(files, collapse = "\n  "))
+      paste(files, collapse = "\n  "))
   }
 
   if (!dir.exists(path)) dir.create(path)
 
-  map2(blob_shas$sha, blob_shas$path, function(blob, file) {
-    gh_git_blob(sha = blob, repo = repo, binary = TRUE, token = token, api = api, ...) %>%
-      writeBin(file.path(path, basename(file)))
-  })
+  for (blob in names(blob_paths)) {
+    gh_download_binary(
+      gh_url("repos", repo, "git/blobs", blob, api = api),
+      path = file.path(path, basename(blob_paths[[blob]])), token = token)
+  }
 
   invisible(path)
 }
 
 #  FUNCTION: gh_source ------------------------------------------------------------------------
+#
 #' Source an R file
 #'
 #' url{https://developer.github.com/v3/git/blobs/#get-a-blob}
@@ -305,8 +355,11 @@ gh_save <- function(
 #' @param api (string, optional) The URL of GitHub's API. Default: the value stored in the
 #'   environment variable \code{"GITHUB_API_URL"} or \code{"https://api.github.com"}.
 #' @param ... Parameters passed to \code{\link{source}}.
+#'
 #' @return The result of sourcing the file.
+#'
 #' @export
+#'
 gh_source <- function(
   file,
   repo,
@@ -315,11 +368,11 @@ gh_source <- function(
   api   = getOption("github.api"),
   ...)
 {
-  assert_that(is.string(file))
-  assert_that(is.string(repo) && identical(str_count(repo, "/"), 1L))
-  assert_that(is.string(ref))
-  assert_that(is.string(token) && identical(str_length(token), 40L))
-  assert_that(is.string(api))
+  stopifnot(is_string(file))
+  stopifnot(is_repo(repo))
+  stopifnot(is_string(ref))
+  stopifnot(is_sha(token))
+  stopifnot(is_url(api))
 
   path <- file.path(tempdir(), "gh_source")
   on.exit(unlink(path, recursive = TRUE))
