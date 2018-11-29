25940b98082f9ef3a86bbd45ee278456ca5a3ebc
Chad <Chad.Goymer@gmail.com>
2018-11-29 07:57:21 +0000

    added create_tree

--- R/git-trees.R
+++ R/git-trees.R
@@ -79,3 +79,104 @@ view_trees <- function(
   info("Done")
   trees_tbl
 }
+
+#  FUNCTION: create_tree ----------------------------------------------------------------------
+#
+#' Create a tree.
+#'
+#' This function creates the specified tree within a repository in GitHub. The tree is either
+#' specified by supplying SHAs of existing blobs and trees, or by specifying the text contents
+#' of files, which are uploaded as blobs.
+#'
+#' <https://developer.github.com/v3/git/trees/#create-a-tree>
+#'
+#' @param repo (string) The repository specified in the format: `owner/repo`.
+#' @param paths (character) The paths to the files or directories.
+#' @param modes (character) The file modes; one of `"100644"` for file (blob), `"100755"` for
+#'   executable (blob), `"040000"` for subdirectory (tree), `"160000"` for submodule (commit), or
+#'   `"120000"` for a blob that specifies the path of a symlink.
+#' @param types (character) The object types. Either `"blob"`, `"tree"`, or `"commit"`.
+#' @param shas (character) The SHAs of the objects in the tree.
+#' @param contents (character) The contents of each file. GitHub will write this blob out and use
+#'   that SHA for this entry. Use either this, or sha.
+#' @param base_tree (string, optional) The SHAs of the tree you want to update with new data.
+#'   Default: `NA`.
+#' @param token (string, optional) The personal access token for GitHub authorisation. Default:
+#'   value stored in the environment variable `GITHUB_TOKEN` (or `GITHUB_PAT`) or in the
+#'   R option `"github.token"`.
+#' @param api (string, optional) The URL of GitHub's API. Default: the value stored in the
+#'   environment variable `GITHUB_API` or in the R option `"github.api"`.
+#' @param ... Parameters passed to [gh_request()].
+#'
+#' @return A tibble describing the trees, with the following columns
+#'   (see [GitHub's documentation](https://developer.github.com/v3/git/refs/) for more details):
+#'   - **sha**: The SHA of the tree.
+#'   - **url**: The URL to get the tree details from GitHub.
+#'
+#' @export
+#'
+create_tree <- function(
+  repo,
+  paths,
+  modes,
+  types,
+  shas      = NA,
+  contents  = NA,
+  base_tree = NA,
+  token     = getOption("github.token"),
+  api       = getOption("github.api"),
+  ...)
+{
+  assert(is_repo(repo))
+  assert(is_character(paths))
+  assert(is_character(modes) && all(modes %in% c("100644", "100755", "040000", "160000", "120000")))
+  assert(identical(length(modes), 1L) || identical(length(modes), length(paths)))
+  assert(is_character(types) && all(types %in% c("blob", "tree", "commit")))
+  assert(identical(length(types), 1L) || identical(length(types), length(paths)))
+  assert(is_na(shas) || all(map_vec(shas, is_sha)))
+  assert(is_na(shas) || (identical(length(shas), 1L) || identical(length(shas), length(paths))))
+  assert(is_na(contents) || is_character(contents))
+  assert(is_na(contents) || (identical(length(contents), 1L) || identical(length(contents), length(paths))))
+  assert(is_na(base_tree) || all(map_vec(base_tree, is_sha)))
+  assert(is_na(base_tree) || (identical(length(base_tree), 1L)) || identical(length(base_tree), length(paths)))
+  assert(is_sha(token))
+  assert(is_url(api))
+
+  params <- tibble(
+    path      = paths,
+    mode      = modes,
+    type      = types,
+    sha       = shas,
+    content   = contents,
+    base_tree = base_tree)
+
+  tree <- pmap(params, use_names = FALSE, function(...) remove_missing(list(...)))
+
+  payload <- list(
+    base_tree = base_tree,
+    tree = tree) %>%
+    remove_missing()
+
+  info("Posting tree to repository '", repo, "'")
+  tree_list <- tryCatch({
+    gh_request(
+      "POST", gh_url("repos", repo, "git/trees", api = api),
+      payload = payload, token = token, ...)
+  }, error = function(e) {
+    error("Creation of tree failed: ", e$message)
+  })
+
+  info("Transforming results")
+  tree_tbl <- bind_fields(tree_list$tree, list(
+    tree_sha = "",
+    tree_url = "",
+    path     = c("path", as = "character"),
+    type     = c("type", as = "character"),
+    sha      = c("sha",  as = "character"),
+    size     = c("size", as = "integer"),
+    url      = c("url",  as = "character"))) %>%
+    mutate(tree_sha = tree_list$sha, tree_url = tree_list$url)
+
+  info("Done")
+  tree_tbl
+}
--- tests/testthat/test-git-trees.R
+++ tests/testthat/test-git-trees.R
@@ -25,3 +25,68 @@ test_that("view_trees returns a tibble of information about the files in commits
       "fea7e317e8d40f5de939e9d183a964a72f14b2c1",
       "72b5faa9dc9e4bba87108bf302a5b453e985feec"))
 })
+
+# TEST: create_tree ---------------------------------------------------------------------------
+
+test_that("create_tree creates a new tree of the specified structure", {
+  created_one_tree_one_object <- create_tree(
+    repo  = "ChadGoymer/test-githapi",
+    paths = "README-copy.md",
+    modes = "100644",
+    types = "blob",
+    shas  = "72b5faa9dc9e4bba87108bf302a5b453e985feec")
+
+  expect_is(created_one_tree_one_object, "tbl")
+  expect_identical(
+    map_vec(created_one_tree_one_object, function(field) class(field)[[1]]),
+    c(tree_sha = "character",
+      tree_url = "character",
+      path     = "character",
+      type     = "character",
+      sha      = "character",
+      size     = "integer",
+      url      = "character"))
+
+  viewed_one_tree_one_object <- view_trees("ChadGoymer/test-githapi", created_one_tree_one_object$tree_sha)
+
+  expect_identical(viewed_one_tree_one_object$path, "README-copy.md")
+  expect_identical(viewed_one_tree_one_object$type, "blob")
+  expect_identical(viewed_one_tree_one_object$sha, "72b5faa9dc9e4bba87108bf302a5b453e985feec")
+
+  created_one_tree_two_objects <- create_tree(
+    repo  = "ChadGoymer/test-githapi",
+    paths = c("test-root.txt", "test-dir"),
+    modes = c("100644", "040000"),
+    types = c("blob", "tree"),
+    shas  = c("fea7e317e8d40f5de939e9d183a964a72f14b2c1", created_one_tree_one_object$tree_sha))
+
+  expect_is(created_one_tree_two_objects, "tbl")
+  expect_identical(
+    map_vec(created_one_tree_two_objects, function(field) class(field)[[1]]),
+    c(tree_sha = "character",
+      tree_url = "character",
+      path     = "character",
+      type     = "character",
+      sha      = "character",
+      size     = "integer",
+      url      = "character"))
+
+  viewed_one_tree_two_objects <- view_trees(
+    repo = "ChadGoymer/test-githapi",
+    created_one_tree_two_objects$tree_sha[[1]])
+
+  expect_true(all(c("test-root.txt", "test-dir") %in% viewed_one_tree_two_objects$path))
+
+  expect_identical(
+    viewed_one_tree_two_objects %>% filter(path == "test-root.txt") %>% pull(type),
+    "blob")
+  expect_identical(
+    viewed_one_tree_two_objects %>% filter(path == "test-dir") %>% pull(type),
+    "tree")
+  expect_identical(
+    viewed_one_tree_two_objects %>% filter(path == "test-root.txt") %>% pull(sha),
+    "fea7e317e8d40f5de939e9d183a964a72f14b2c1")
+  expect_identical(
+    viewed_one_tree_two_objects %>% filter(path == "test-dir") %>% pull(sha),
+    created_one_tree_one_object$tree_sha)
+})
