edaf3584300d5fcfdc6140c875dfe4a067b61970
Chad <Chad.Goymer@gmail.com>
2018-11-30 18:09:49 +0000

    added update_branches

--- R/git-branches.R
+++ R/git-branches.R
@@ -162,3 +162,78 @@ create_branches <- function(
   info("Done")
   branches_tbl
 }
+
+#  FUNCTION: update_branches ------------------------------------------------------------------
+#
+#' Update branches to new commits.
+#'
+#' This function updates the specified branches to point at new commits within a repository in
+#' GitHub.
+#'
+#' <https://developer.github.com/v3/git/refs/#update-a-reference>
+#'
+#' @param repo (string) The repository specified in the format: `owner/repo`.
+#' @param branches (character) The branch names.
+#' @param shas (character) The SHAs of the commits to update the branch to.
+#' @param token (string, optional) The personal access token for GitHub authorisation. Default:
+#'   value stored in the environment variable `GITHUB_TOKEN` (or `GITHUB_PAT`) or in the
+#'   R option `"github.token"`.
+#' @param api (string, optional) The URL of GitHub's API. Default: the value stored in the
+#'   environment variable `GITHUB_API` or in the R option `"github.api"`.
+#' @param ... Parameters passed to [gh_request()].
+#'
+#' @return A tibble describing the branches, with the following columns
+#'   (see [GitHub's documentation](https://developer.github.com/v3/git/refs/) for more details):
+#'   - **name**: The name of the branch.
+#'   - **ref**: The full git reference for the branch.
+#'   - **url**: The URL to get the branch details from GitHub.
+#'   - **object_sha**: The SHA of the object at the HEAD of the branch.
+#'   - **object_type**: The type of the object at the HEAD of the branch.
+#'   - **object_url**: The URL to get the object details from GitHub.
+#'
+#' @export
+#'
+update_branches <- function(
+  repo,
+  branches,
+  shas,
+  token = getOption("github.token"),
+  api   = getOption("github.api"),
+  ...)
+{
+  assert(is_repo(repo))
+  assert(is_character(branches))
+  assert(is_character(shas))
+  assert(is_sha(token))
+  assert(is_url(api))
+
+  branches_list <- pmap(list(branches, shas), function(branch, sha) {
+    info("Updating branch '", branch, "' in repository '", repo, "'")
+    tryCatch({
+      gh_request(
+        "PATCH", gh_url("repos", repo, "git/refs/heads", branch, api = api),
+        payload = list(sha = sha, force = TRUE),
+        token = token, ...)
+    }, error = function(e) {
+      info("Branch '", branch, "' failed!")
+      e
+    })
+  })
+
+  if (any(map_vec(branches_list, is, "error"))) {
+    collate_errors(branches_list, "update_branches() failed!")
+  }
+
+  info("Transforming results")
+  branches_tbl <- bind_fields(branches_list[!map_vec(branches_list, is_null)], list(
+    name        = "",
+    ref         = c("ref",            as = "character"),
+    url         = c("url",            as = "character"),
+    object_sha  = c("object", "sha",  as = "character"),
+    object_type = c("object", "type", as = "character"),
+    object_url  = c("object", "url",  as = "character"))) %>%
+    mutate(name = basename(.data$ref))
+
+  info("Done")
+  branches_tbl
+}
--- tests/testthat/test-git-branches.R
+++ tests/testthat/test-git-branches.R
@@ -53,4 +53,24 @@ test_that("create_branches creates some branches, view_branches retreives them a
   expect_identical(
     viewed_branches$object_sha,
     c("cbd94cf24a4c62761b3ae59ca3c69f868591cf7d", "310c21d3f1601a46e014e68e94814b23406bf574"))
+
+  updated_branches <- update_branches(
+    branches = c("aaa", "bbb"),
+    shas = c("32d3c5c4f6aba7ae9679480407e1b9f94ad04843", "68f01be0dad53f366337c9d87fad939b2a2853c8"),
+    repo = "ChadGoymer/test-githapi")
+
+  expect_is(updated_branches, "tbl")
+  expect_identical(
+    map_vec(updated_branches, function(field) class(field)[[1]]),
+    c(name        = "character",
+      ref         = "character",
+      url         = "character",
+      object_sha  = "character",
+      object_type = "character",
+      object_url  = "character"))
+
+  expect_identical(updated_branches$ref, c("refs/heads/aaa", "refs/heads/bbb"))
+  expect_identical(
+    updated_branches$object_sha,
+    c("32d3c5c4f6aba7ae9679480407e1b9f94ad04843", "68f01be0dad53f366337c9d87fad939b2a2853c8"))
 })
