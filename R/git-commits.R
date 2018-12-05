dec692ad99db12dc8df2d1d51d7d269f0093f5ef
Chad <Chad.Goymer@gmail.com>
2018-12-05 07:51:52 +0000

    added upload_commit

--- R/git-commits.R
+++ R/git-commits.R
@@ -202,3 +202,144 @@ create_commit <- function(
   info("Done")
   commit_tbl
 }
+
+#  FUNCTION: upload_commit --------------------------------------------------------------------
+#
+#' Upload a directory of files and create a commit on a branch
+#'
+#' This function uploads all the files in the specified directory path (using [upload_tree()]).
+#' It the creates a new commit (using [create_commit()]) and sets it as the head of the
+#' specified branch (using [create_branches()]).
+#'
+#' The default behaviour is to treat the directory as containing the entire set of files and
+#' completely `replace` the previous commit with this one. This means that if a previously
+#' existing file is not in the directory it will be deleted in the commit. Alternatively, if
+#' you just want to update a few files, you can specify `replace = FALSE`, in which case
+#' the files in the directory will replace the ones in the previous commit, and all other
+#' files will remain unchanged. This is useful if you have a lot of files in the commit.
+#'
+#' <https://developer.github.com/v3/git/blobs/#create-a-blob>
+#'
+#' <https://developer.github.com/v3/git/trees/#create-a-tree>
+#'
+#' <https://developer.github.com/v3/git/commits/#create-a-commit>
+#'
+#' <https://developer.github.com/v3/git/refs/#create-a-reference>
+#'
+#' @param repo (string) The repository specified in the format: `owner/repo`.
+#' @param branch (string) The name of the branch to set the commit as head.
+#' @param message (string) The commit message
+#' @param path (string) The path to the directory, which is to be uploaded as a tree.
+#' @param parents (character) The SHAs of the commits that were the parents of this commit. If
+#'   `NA`, the commit will be written as a root commit. For a single parent, one SHA should be
+#'   provided; for a merge commit, a character vector of more than one should be provided.
+#'   Default: `NA`.
+#' @param replace (logical, optional) Whether the commit should replace the previous one or
+#'   just update the files within the directory (see description). Default: `TRUE`.
+#' @param committer (list, optional) The name and email address of the committer. This needs
+#'   to be specified as a named list, e.g. `list(name = "Bob Smith", email = "bob.smith@acme.com")`.
+#'   If `NA` then the authenticated user is used. Default: `NA`
+#' @param author (list, optional) The name and email address of the author. This needs to be
+#'   specified as a named list, e.g. `list(name = "Bob Smith", email = "bob.smith@acme.com")`.
+#'   If `NA` then the authenticated user is used. Default: `NA`
+#' @param ignore (character) A character vector of regular expressions. If any of these are
+#'   detected in a file name they are not uploaded. Default: `"\\.git"`, `"\\.Rproj\\.user"`,
+#'   `"\\.Rhistory"`, `"\\.RData"` & `"\\.Ruserdata"`
+#' @param token (string, optional) The personal access token for GitHub authorisation. Default:
+#'   value stored in the environment variable `GITHUB_TOKEN` (or `GITHUB_PAT`) or in the
+#'   R option `"github.token"`.
+#' @param api (string, optional) The URL of GitHub's API. Default: the value stored in the
+#'   environment variable `GITHUB_API` or in the R option `"github.api"`.
+#' @param ... Parameters passed to [gh_request()].
+#'
+#' @return A tibble describing the commit, with the following columns
+#'   (see [GitHub's documentation](https://developer.github.com/v3/repos/commits/)
+#'   for more details):
+#'   - **sha**: The SHA of the commit.
+#'   - **message**: The commit message recorded in GitHub.
+#'   - **author_name**: The author of the commit.
+#'   - **author_email**: The author's email address.
+#'   - **committer_name**: The committer of the commit.
+#'   - **committer_email**: The committer's email address.
+#'   - **date**: The date and time of the commit.
+#'   - **url**: The URL to get the commit details from GitHub.
+#'   - **tree_sha**: The SHA of the git tree.
+#'   - **tree_url**: The URL to get the tree details from GitHub.
+#'   - **parent_sha**: The SHA(s) of the parent commit(s).
+#'   - **parent_url**: The URL(s) to get the parent's details from GitHub.
+#'
+#' @export
+#'
+upload_commit <- function(
+  repo,
+  branch,
+  message,
+  path,
+  parents   = NA,
+  replace   = TRUE,
+  committer = NA,
+  author    = NA,
+  ignore    = c("\\.git", "\\.Rproj\\.user", "\\.Rhistory", "\\.RData", "\\.Ruserdata"),
+  token     = getOption("github.token"),
+  api       = getOption("github.api"),
+  ...)
+{
+  assert(is_repo(repo))
+  assert(is_string(branch))
+  assert(is_string(message))
+  assert(is_readable(path))
+  assert(is_character(parents))
+  assert(is_boolean(replace))
+  assert(is_na(committer) || (
+    is_list(committer) && identical(names(committer), c("name", "email")) &&
+      is_string(committer$name) && is_string(committer$email)))
+  assert(is_na(author) || (
+    is_list(author) && identical(names(author), c("name", "email")) &&
+      is_string(author$name) && is_string(author$email)))
+  assert(is_character(ignore))
+  assert(is_sha(token))
+  assert(is_url(api))
+
+  if (replace) {
+    base_tree <- NA
+  } else {
+    parent_commit <- view_history(repo, parents[[1]], n_max = 1)
+    base_tree <- parent_commit$tree_sha
+  }
+
+  tree <- upload_tree(
+    repo      = repo,
+    path      = path,
+    base_tree = base_tree,
+    ignore    = ignore,
+    token     = token,
+    api       = api)
+
+  commit <- create_commit(
+    repo      = repo,
+    message   = message,
+    tree      = tree$tree_sha[[1]],
+    parents   = parents,
+    committer = committer,
+    author    = author,
+    token     = token,
+    api       = api)
+
+  if (branch_exists(repo = repo, branch = branch, token = token, api = api)) {
+    update_branches(
+      repo      = repo,
+      branches  = branch,
+      shas      = commit$sha,
+      token     = token,
+      api       = api)
+  } else {
+    create_branches(
+      repo      = repo,
+      branches  = branch,
+      shas      = commit$sha,
+      token     = token,
+      api       = api)
+  }
+
+  commit
+}
--- tests/testthat/test-git-commits.R
+++ tests/testthat/test-git-commits.R
@@ -59,3 +59,149 @@ test_that("create_commit creates a new commit in a repository", {
   viewed_commit <- view_commits("ChadGoymer/test-githapi", created_commit$sha)
   expect_identical(created_commit, viewed_commit)
 })
+
+# TEST: upload_commit -------------------------------------------------------------------------
+
+test_that("upload_commit uploads files and directory structure to github", {
+  skip("Does not work in Travis")
+
+  flat_commit <- upload_commit(
+    repo    = "ChadGoymer/test-githapi",
+    branch  = "master",
+    message = "Test commit made with upload_commit",
+    path    = system.file("test-data/upload-tree/test-dir", package = "githapi"),
+    parents = "master")
+
+  expect_is(flat_commit, "tbl")
+  expect_identical(
+    map_vec(flat_commit, function(field) class(field)[[1]]),
+    c(sha             = "character",
+      message         = "character",
+      author_name     = "character",
+      author_email    = "character",
+      committer_name  = "character",
+      committer_email = "character",
+      date            = "POSIXct",
+      url             = "character",
+      tree_sha        = "character",
+      tree_url        = "character",
+      parent_sha      = "list",
+      parent_url      = "list"))
+
+  expect_identical(flat_commit$message, c("Test commit made with upload_commit"))
+
+  master_files <- view_trees("ChadGoymer/test-githapi", shas = flat_commit$sha)
+  expect_identical(master_files$path, c("file-in-dir-1.txt", "file-in-dir-2.txt"))
+
+  recursive_commit <- upload_commit(
+    repo    = "ChadGoymer/test-githapi",
+    branch  = "master",
+    message = "Recursive commit made with upload_commit",
+    path    = system.file("test-data/upload-tree", package = "githapi"),
+    parents = "master")
+
+  expect_is(recursive_commit, "tbl")
+  expect_identical(
+    map_vec(recursive_commit, function(field) class(field)[[1]]),
+    c(sha             = "character",
+      message         = "character",
+      author_name     = "character",
+      author_email    = "character",
+      committer_name  = "character",
+      committer_email = "character",
+      date            = "POSIXct",
+      url             = "character",
+      tree_sha        = "character",
+      tree_url        = "character",
+      parent_sha      = "list",
+      parent_url      = "list"))
+
+  expect_identical(recursive_commit$message, c("Recursive commit made with upload_commit"))
+
+  master_files <- view_trees("ChadGoymer/test-githapi", shas = recursive_commit$sha)
+  expect_identical(
+    master_files$path,
+    c("README.md",
+      "test-dir",
+      "test-dir/file-in-dir-1.txt",
+      "test-dir/file-in-dir-2.txt",
+      "test-file.txt"))
+
+  temp_path <- file.path(tempdir(), "test-git-commits")
+  if (!dir.exists(temp_path)) dir.create(temp_path)
+  on.exit(unlink(temp_path))
+
+  download_files(
+    repo     = "ChadGoymer/test-githapi",
+    paths    = "test-file.txt",
+    location = temp_path,
+    ref      = "master")
+
+  temp_file <- file.path(temp_path, "test-file.txt")
+  now <- as.character(Sys.time())
+  writeLines(c("This file was updated:", now), temp_file)
+
+  updated_commit <- upload_commit(
+    repo      = "ChadGoymer/test-githapi",
+    branch    = "master",
+    message   = "Update commit made with upload_commit",
+    path      = temp_path,
+    parents   = "master",
+    replace   = FALSE)
+
+  expect_is(updated_commit, "tbl")
+  expect_identical(
+    map_vec(updated_commit, function(field) class(field)[[1]]),
+    c(sha             = "character",
+      message         = "character",
+      author_name     = "character",
+      author_email    = "character",
+      committer_name  = "character",
+      committer_email = "character",
+      date            = "POSIXct",
+      url             = "character",
+      tree_sha        = "character",
+      tree_url        = "character",
+      parent_sha      = "list",
+      parent_url      = "list"))
+
+  expect_identical(updated_commit$message, c("Update commit made with upload_commit"))
+
+  master_files <- view_trees("ChadGoymer/test-githapi", shas = updated_commit$sha)
+  expect_identical(
+    master_files$path,
+    c("README.md",
+      "test-dir",
+      "test-dir/file-in-dir-1.txt",
+      "test-dir/file-in-dir-2.txt",
+      "test-file.txt"))
+
+  file_contents <- read_files("ChadGoymer/test-githapi", "test-file.txt", ref = "master")
+  expect_match(file_contents, now)
+
+  new_branch_commit <- upload_commit(
+    repo    = "ChadGoymer/test-githapi",
+    branch  = "test-upload-commit",
+    message = "Commit made on new branch with upload_commit",
+    path    = system.file("test-data/upload-tree", package = "githapi"),
+    parents = "master")
+  on.exit(delete_branches(repo = "ChadGoymer/test-githapi", branch = "test-upload-commit"))
+
+  expect_is(new_branch_commit, "tbl")
+  expect_identical(
+    map_vec(new_branch_commit, function(field) class(field)[[1]]),
+    c(sha             = "character",
+      message         = "character",
+      author_name     = "character",
+      author_email    = "character",
+      committer_name  = "character",
+      committer_email = "character",
+      date            = "POSIXct",
+      url             = "character",
+      tree_sha        = "character",
+      tree_url        = "character",
+      parent_sha      = "list",
+      parent_url      = "list"))
+
+  expect_identical(new_branch_commit$message, c("Commit made on new branch with upload_commit"))
+})
