c6ab911c29732202f5eb4d885604717f5ddf626d
Chad <Chad.Goymer@gmail.com>
2019-02-11 08:38:02 +0000

    refactored git commits

--- tests/testthat/test-git-commits.R
+++ tests/testthat/test-git-commits.R
@@ -4,8 +4,8 @@ context("git commits")
 
 test_that("view_commits returns a tibble of information about the commits", {
   commits <- view_commits(
-    repo = "ChadGoymer/test-githapi",
-    shas = c("cbd94cf24a4c62761b3ae59ca3c69f868591cf7d", "ccb62ec75de7e40c689be427cd038c8a1a9d3c44"))
+    shas = c("cbd94cf24a4c62761b3ae59ca3c69f868591cf7d", "ccb62ec75de7e40c689be427cd038c8a1a9d3c44"),
+    repo = "ChadGoymer/test-githapi")
 
   expect_is(commits, "tbl")
   expect_identical(
@@ -35,10 +35,10 @@ test_that("view_commits returns a tibble of information about the commits", {
 
 test_that("create_commit creates a new commit in a repository", {
   created_commit <- create_commit(
-    repo    = "ChadGoymer/test-githapi",
     message = "This is created with create_commit()",
     tree    = "bbac77ba8fc1afa9a815a0cc8fc17e221cb0c027",
-    parents = "master")
+    parents = "master",
+    repo    = "ChadGoymer/test-githapi")
 
   expect_is(created_commit, "tbl")
   expect_identical(
@@ -56,8 +56,32 @@ test_that("create_commit creates a new commit in a repository", {
       parent_sha      = "list",
       parent_url      = "list"))
 
-  viewed_commit <- view_commits("ChadGoymer/test-githapi", created_commit$sha)
+  viewed_commit <- view_commits(created_commit$sha, "ChadGoymer/test-githapi")
   expect_identical(created_commit, viewed_commit)
+
+  orphan_commit <- create_commit(
+    message = "This is an orphan created with create_commit()",
+    tree    = "bbac77ba8fc1afa9a815a0cc8fc17e221cb0c027",
+    repo    = "ChadGoymer/test-githapi")
+
+  expect_is(orphan_commit, "tbl")
+  expect_identical(
+    map_vec(orphan_commit, function(field) class(field)[[1]]),
+    c(sha             = "character",
+      message         = "character",
+      author_name     = "character",
+      author_email    = "character",
+      committer_name  = "character",
+      committer_email = "character",
+      date            = "POSIXct",
+      url             = "character",
+      tree_sha        = "character",
+      tree_url        = "character",
+      parent_sha      = "list",
+      parent_url      = "list"))
+
+  expect_null(orphan_commit$parent_sha[[1]])
+  expect_null(orphan_commit$parent_url[[1]])
 })
 
 # TEST: upload_commit -------------------------------------------------------------------------
@@ -66,11 +90,11 @@ test_that("upload_commit uploads files and directory structure to github", {
   skip("Does not work in Travis")
 
   flat_commit <- upload_commit(
-    repo    = "ChadGoymer/test-githapi",
     branch  = "master",
     message = "Test commit made with upload_commit",
     path    = system.file("test-data/upload-tree/test-dir", package = "githapi"),
-    parents = "master")
+    parents = "master",
+    repo    = "ChadGoymer/test-githapi")
 
   expect_is(flat_commit, "tbl")
   expect_identical(
@@ -88,17 +112,17 @@ test_that("upload_commit uploads files and directory structure to github", {
       parent_sha      = "list",
       parent_url      = "list"))
 
-  expect_identical(flat_commit$message, c("Test commit made with upload_commit"))
+  expect_identical(flat_commit$message, "Test commit made with upload_commit")
 
-  master_files <- view_trees("ChadGoymer/test-githapi", shas = flat_commit$sha)
+  master_files <- view_trees(flat_commit$sha, "ChadGoymer/test-githapi")
   expect_identical(master_files$path, c("file-in-dir-1.txt", "file-in-dir-2.txt"))
 
   recursive_commit <- upload_commit(
-    repo    = "ChadGoymer/test-githapi",
     branch  = "master",
     message = "Recursive commit made with upload_commit",
     path    = system.file("test-data/upload-tree", package = "githapi"),
-    parents = "master")
+    parents = "master",
+    repo    = "ChadGoymer/test-githapi")
 
   expect_is(recursive_commit, "tbl")
   expect_identical(
@@ -116,9 +140,9 @@ test_that("upload_commit uploads files and directory structure to github", {
       parent_sha      = "list",
       parent_url      = "list"))
 
-  expect_identical(recursive_commit$message, c("Recursive commit made with upload_commit"))
+  expect_identical(recursive_commit$message, "Recursive commit made with upload_commit")
 
-  master_files <- view_trees("ChadGoymer/test-githapi", shas = recursive_commit$sha)
+  master_files <- view_trees(recursive_commit$sha, "ChadGoymer/test-githapi")
   expect_identical(
     master_files$path,
     c("README.md",
@@ -129,24 +153,24 @@ test_that("upload_commit uploads files and directory structure to github", {
 
   temp_path <- file.path(tempdir(), "test-git-commits")
   if (!dir.exists(temp_path)) dir.create(temp_path)
-  on.exit(unlink(temp_path))
+  on.exit(unlink(temp_path, recursive = TRUE))
 
   download_files(
-    repo     = "ChadGoymer/test-githapi",
     paths    = "test-file.txt",
     location = temp_path,
-    ref      = "master")
+    ref      = "master",
+    repo     = "ChadGoymer/test-githapi")
 
   temp_file <- file.path(temp_path, "test-file.txt")
   now <- as.character(Sys.time())
   writeLines(c("This file was updated:", now), temp_file)
 
   updated_commit <- upload_commit(
-    repo      = "ChadGoymer/test-githapi",
     branch    = "master",
     message   = "Update commit made with upload_commit",
     path      = temp_path,
     parents   = "master",
+    repo      = "ChadGoymer/test-githapi",
     replace   = FALSE)
 
   expect_is(updated_commit, "tbl")
@@ -165,9 +189,9 @@ test_that("upload_commit uploads files and directory structure to github", {
       parent_sha      = "list",
       parent_url      = "list"))
 
-  expect_identical(updated_commit$message, c("Update commit made with upload_commit"))
+  expect_identical(updated_commit$message, "Update commit made with upload_commit")
 
-  master_files <- view_trees("ChadGoymer/test-githapi", shas = updated_commit$sha)
+  master_files <- view_trees(updated_commit$sha, "ChadGoymer/test-githapi")
   expect_identical(
     master_files$path,
     c("README.md",
@@ -176,16 +200,16 @@ test_that("upload_commit uploads files and directory structure to github", {
       "test-dir/file-in-dir-2.txt",
       "test-file.txt"))
 
-  file_contents <- read_files("ChadGoymer/test-githapi", "test-file.txt", ref = "master")
+  file_contents <- read_files("test-file.txt", ref = "master", repo = "ChadGoymer/test-githapi")
   expect_match(file_contents, now)
 
   new_branch_commit <- upload_commit(
-    repo    = "ChadGoymer/test-githapi",
     branch  = "test-upload-commit",
     message = "Commit made on new branch with upload_commit",
     path    = system.file("test-data/upload-tree", package = "githapi"),
-    parents = "master")
-  on.exit(delete_branches(repo = "ChadGoymer/test-githapi", branch = "test-upload-commit"))
+    parents = "master",
+    repo    = "ChadGoymer/test-githapi")
+  on.exit(delete_branches("test-upload-commit", "ChadGoymer/test-githapi"), add = TRUE)
 
   expect_is(new_branch_commit, "tbl")
   expect_identical(
@@ -203,12 +227,16 @@ test_that("upload_commit uploads files and directory structure to github", {
       parent_sha      = "list",
       parent_url      = "list"))
 
-  expect_identical(new_branch_commit$message, c("Commit made on new branch with upload_commit"))
+  expect_identical(new_branch_commit$message, "Commit made on new branch with upload_commit")
 })
 
-# TEST: commit_exists -------------------------------------------------------------------------
+# TEST: commits_exist -------------------------------------------------------------------------
 
-test_that("commit_exists returns TRUE or FALSE depending on whether the commit exists in the repo", {
-  expect_true(commit_exists("ChadGoymer/test-githapi", "cbd94cf24a4c62761b3ae59ca3c69f868591cf7d"))
-  expect_false(commit_exists("ChadGoymer/test-githapi", "0000000000000000000000000000000000000000"))
+test_that("commits_exist returns TRUE or FALSE depending on whether the commit exists in the repo", {
+  expect_true(commits_exist("cbd94cf24a4c62761b3ae59ca3c69f868591cf7d", "ChadGoymer/test-githapi"))
+  expect_false(commits_exist("0000000000000000000000000000000000000000", "ChadGoymer/test-githapi"))
+
+  expect_identical(
+    commits_exist(c("cbd94cf24a4c62761b3ae59ca3c69f868591cf7d", "0000000000000000000000000000000000000000"), "ChadGoymer/test-githapi"),
+    c(`cbd94cf24a4c62761b3ae59ca3c69f868591cf7d` = TRUE, `0000000000000000000000000000000000000000` = FALSE))
 })
