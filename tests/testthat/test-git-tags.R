87974da9160cb1b17c1f853257288c4f35fbb67e
Chad <Chad.Goymer@gmail.com>
2019-02-11 08:39:51 +0000

    refactored git tags

--- tests/testthat/test-git-tags.R
+++ tests/testthat/test-git-tags.R
@@ -3,12 +3,18 @@ context("git tags")
 # TEST: view_tags, create_tags & delete_tags --------------------------------------------------
 
 test_that("create_tags creates some tags, view_tags retreives them and delete_tags deletes them", {
-  all_tags <- view_tags(repo = "ChadGoymer/test-githapi")
+  all_tags <- view_tags("ChadGoymer/test-githapi")
 
   expect_is(all_tags, "tbl")
   expect_identical(
-    names(all_tags),
-    c("name", "ref", "url", "object_sha", "object_type", "object_url"))
+    map_vec(all_tags, function(field) class(field)[[1]]),
+    c(name        = "character",
+      ref         = "character",
+      url         = "character",
+      object_sha  = "character",
+      object_type = "character",
+      object_url  = "character"))
+
   expect_true("0.0.0" %in% all_tags$name)
   expect_identical(
     filter(all_tags, name == "0.0.0") %>% pull(object_sha),
@@ -21,19 +27,31 @@ test_that("create_tags creates some tags, view_tags retreives them and delete_ta
 
   expect_is(created_tags, "tbl")
   expect_identical(
-    names(created_tags),
-    c("name", "ref", "url", "object_sha", "object_type", "object_url"))
+    map_vec(created_tags, function(field) class(field)[[1]]),
+    c(name        = "character",
+      ref         = "character",
+      url         = "character",
+      object_sha  = "character",
+      object_type = "character",
+      object_url  = "character"))
+
   expect_identical(created_tags$ref, c("refs/tags/aaa", "refs/tags/bbb"))
   expect_identical(
     created_tags$object_sha,
     c("cbd94cf24a4c62761b3ae59ca3c69f868591cf7d", "310c21d3f1601a46e014e68e94814b23406bf574"))
 
-  viewed_tags <- view_tags("ChadGoymer/test-githapi", c("aaa", "bbb"))
+  viewed_tags <- view_tags(c("aaa", "bbb"), "ChadGoymer/test-githapi")
 
   expect_is(viewed_tags, "tbl")
   expect_identical(
-    names(viewed_tags),
-    c("name", "ref", "url", "object_sha", "object_type", "object_url"))
+    map_vec(viewed_tags, function(field) class(field)[[1]]),
+    c(name        = "character",
+      ref         = "character",
+      url         = "character",
+      object_sha  = "character",
+      object_type = "character",
+      object_url  = "character"))
+
   expect_identical(viewed_tags$name, c("aaa", "bbb"))
   expect_identical(
     viewed_tags$object_sha,
@@ -46,23 +64,33 @@ test_that("create_tags creates some tags, view_tags retreives them and delete_ta
 
   expect_is(updated_tags, "tbl")
   expect_identical(
-    names(updated_tags),
-    c("name", "ref", "url", "object_sha", "object_type", "object_url"))
+    map_vec(updated_tags, function(field) class(field)[[1]]),
+    c(name        = "character",
+      ref         = "character",
+      url         = "character",
+      object_sha  = "character",
+      object_type = "character",
+      object_url  = "character"))
+
   expect_identical(updated_tags$ref, c("refs/tags/aaa", "refs/tags/bbb"))
   expect_identical(
     updated_tags$object_sha,
     c("32d3c5c4f6aba7ae9679480407e1b9f94ad04843", "68f01be0dad53f366337c9d87fad939b2a2853c8"))
 
-  delete_results <- delete_tags("ChadGoymer/test-githapi", c("aaa", "bbb"))
+  delete_results <- delete_tags(c("aaa", "bbb"), "ChadGoymer/test-githapi")
 
   expect_identical(delete_results, list(aaa = TRUE, bbb = TRUE))
-  expect_error(view_tags("ChadGoymer/test-githapi", "aaa"), "Not Found")
-  expect_error(view_tags("ChadGoymer/test-githapi", "bbb"), "Not Found")
+  expect_error(suppressWarnings(view_tags("aaa", "ChadGoymer/test-githapi")), "Not Found")
+  expect_error(suppressWarnings(view_tags("bbb", "ChadGoymer/test-githapi")), "Not Found")
 })
 
-# TEST: tag_exists ----------------------------------------------------------------------------
+# TEST: tags_exist ----------------------------------------------------------------------------
 
-test_that("tag_exists returns TRUE or FALSE depending on whether the tag exists in the repo", {
-  expect_true(tag_exists("ChadGoymer/test-githapi", "0.0.0"))
-  expect_false(tag_exists("ChadGoymer/test-githapi", "no-such-tag"))
+test_that("tags_exist returns TRUE or FALSE depending on whether the tag exists in the repo", {
+  expect_true(tags_exist("0.0.0", "ChadGoymer/test-githapi"))
+  expect_false(tags_exist("no-such-tag", "ChadGoymer/test-githapi"))
+
+  expect_identical(
+    tags_exist(c("0.0.0", "no-such-tag"), "ChadGoymer/test-githapi"),
+    c(`0.0.0` = TRUE, `no-such-tag` = FALSE))
 })
