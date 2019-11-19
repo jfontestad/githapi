fdae8811fbd664965c97182595ec8fa6ed9e96fd
Chad Goymer <chad.goymer@gmail.com>
2019-11-19 07:52:55 +0000

    Split out package documentation

---
+++ R/githapi-package.R
@@ -0,0 +1,13 @@
+#  PACKAGE: githapi ---------------------------------------------------------------------------
+#
+#' User-friendly access to the GitHub API for R, consistent with the tidyverse
+#'
+#' Provides a suite of functions which simplify working with GitHub's API.
+#'
+#' @importFrom methods as is
+#' @import purrr tibble dplyr msgr
+#'
+#' @docType package
+#' @name githapi
+#'
+globalVariables(c(".", ".data"))
