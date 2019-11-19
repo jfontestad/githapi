ce14d616f50451c2dc20a9c0d2683073b884889a
Chad Goymer <chad.goymer@gmail.com>
2019-11-19 07:54:22 +0000

    Updated on-load script

--- R/on-load.R
+++ R/on-load.R
@@ -1,30 +1,46 @@
-.onLoad <- function(libname, pkgname) {
-
-  token <- Sys.getenv("GITHUB_TOKEN")
-  if (identical(token, "")) {
-    token <- Sys.getenv("GITHUB_PAT")
-  }
-
-  github_env <- list(
+.onLoad <- function(libname, pkgname)
+{
+  # Set environment variables that do not already exist
+  githapi_env <- list(
     GITHUB_API        = "https://api.github.com",
-    GITHUB_TOKEN      = token,
+    GITHUB_TOKEN      = "",
+    GITHUB_PAT        = "",
     GITHAPI_MSG_LEVEL = "1",
     GITHAPI_MSG_TYPES = "INFO|WARNING|ERROR",
-    GITHAPI_LOGS      = "")
+    GITHAPI_LOGS      = ""
+  )
 
-  toset <- sapply(names(github_env), function(e) identical(Sys.getenv(e), ""))
-  if (any(toset)) do.call(Sys.setenv, github_env[toset])
-  githapi_env <- as.list(Sys.getenv(names(github_env)))
+  toset <- sapply(names(githapi_env), function(e) identical(Sys.getenv(e), ""))
+  if (any(toset)) do.call(Sys.setenv, githapi_env[toset])
 
-  if (!identical(githapi_env$GITHAPI_LOGS, "")) {
+  # Read environment variables, to set as options
+  githapi_env <- as.list(Sys.getenv(names(githapi_env)))
+
+  # Set github token
+  if (identical(githapi_env$GITHUB_TOKEN, ""))
+  {
+    githapi_env$GITHUB_TOKEN <- githapi_env$GITHUB_PAT
+  }
+
+  # Set log file name
+  if (!identical(githapi_env$GITHAPI_LOGS, ""))
+  {
     githapi_env$GITHAPI_LOGS <- paste0(githapi_env$GITHAPI_LOGS, "/", "githapi-", Sys.info()[["login"]], "-", Sys.Date(), ".log")
   }
 
+  # Set R options
   options(
-    github.api    = githapi_env$GITHUB_API,
-    github.token  = githapi_env$GITHUB_TOKEN,
-    msgr.level    = suppressWarnings(as.integer(githapi_env$GITHAPI_MSG_LEVEL)),
-    msgr.types    = strsplit(githapi_env$GITHAPI_MSG_TYPES, split = "\\|")[[1]],
-    msgr.log_path = normalizePath(githapi_env$GITHAPI_LOGS, winslash = "/", mustWork = FALSE))
+    # Base Configuration
+    scipen                    = 999,                         # Prevent printing in scientific notation
+    warning.length            = 8170,                        # Set warning length to maximum
 
+    # GITHUB API
+    github.api                = githapi_env$GITHUB_API,      # The base address of the GitHub API
+    github.token              = githapi_env$GITHUB_TOKEN,    # The GitHub token
+
+    # Message Configuration
+    msgr.msg_level            = suppressWarnings(as.integer(githapi_env$GITHAPI_MSG_LEVEL)),
+    msgr.msg_types            = strsplit(githapi_env$GITHAPI_MSG_TYPES, split = "\\|")[[1]],
+    msgr.log_path             = normalizePath(githapi_env$GITHAPI_LOGS, winslash = "/", mustWork = FALSE)
+  )
 }
