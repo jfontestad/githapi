244f54d1f29c2ea3b6936ae8a72b0a26b5373f91
Chad Goymer <chad.goymer@gmail.com>
2019-11-20 18:00:15 +0000

    Added rlang as dependency and imported integerish functions

--- R/githapi-package.R
+++ R/githapi-package.R
@@ -2,12 +2,22 @@
 #
 #' User-friendly access to the GitHub API for R, consistent with the tidyverse
 #'
-#' Provides a suite of functions which simplify working with GitHub's API.
+#' Provides a suite of functions which simplify working with GitHub's API. The functions
+#' have a constent API and return either lists for single entities and tibbles for
+#' collections.
+#'
+#' @name githapi
+#' @docType package
+#'
+#' @seealso
+#' Useful links:
+#' - \url{https://github.com/ChadGoymer/githapi}
+#' - Report bugs at \url{https://github.com/ChadGoymer/githapi/issues}
+#'
+#' @author Chad Goymer \email{chad.goymer@@gmail.com}
 #'
 #' @importFrom methods as is
+#' @importFrom rlang is_integerish is_scalar_integerish
 #' @import purrr tibble dplyr msgr
-#'
-#' @docType package
-#' @name githapi
 #'
 globalVariables(c(".", ".data"))
