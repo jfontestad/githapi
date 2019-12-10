a3071abb7aa7da0350aeb5f59a269010d504ac7c
Chad <Chad.Goymer@gmail.com>
2019-12-10 18:44:47 +0000

    Added create_project()

--- R/projects.R
+++ R/projects.R
@@ -1,252 +1,99 @@
-#  FUNCTION: gh_project -----------------------------------------------------------------------
+#  FUNCTION: create_project -------------------------------------------------------------------
 #
-#' Get a project
+#' Create a GitHub project
 #'
-#' <https://developer.github.com/v3/projects/#get-a-project>
+#' This function creates a new project in GitHub. The project will be empty so you will need
+#' to add columns and cards separately.
 #'
-#' @param project (integer) The ID of the project in GitHub.
-#' @param token (string, optional) The personal access token for GitHub authorisation. Default:
-#'   value stored in the environment variable `GITHUB_TOKEN` or `GITHUB_PAT`.
-#' @param api (string, optional) The URL of GitHub's API. Default: the value stored in the
-#'   environment variable `GITHUB_API` or `https://api.github.com`.
-#' @param ... Parameters passed to [gh_get()].
+#' You can create a project associated with either a repository or organisation, by
+#' supplying them as an input, as long as you have appropriate permissions. If no repository
+#' or organisation is specified the project is created for the authenticated user.
 #'
-#' @return A list describing the project (see GitHub's API documentation for details).
+#' For more details see the GitHub API documentation:
+#' - <https://developer.github.com/v3/projects/#create-a-repository-project>
+#' - <https://developer.github.com/v3/projects/#create-an-organization-project>
+#' - <https://developer.github.com/v3/projects/#create-a-user-project>
+#'
+#' @param name (string) The name of the project.
+#' @param body (string) The description of the project.
+#' @param repo (string, optional) The repository specified in the format: `owner/repo`.
+#' @param org (string, optional) The name of the organization.
+#' @param ... Parameters passed to [gh_request()].
+#'
+#' @return `create_project()` returns a list of the project properties.
+#'
+#' **Project Properties:**
+#'
+#' - **id**: The ID of the project.
+#' - **number**: The number of the project for the repository, user or organisation.
+#' - **name**: The name given to the project.
+#' - **body**: The description given to the project.
+#' - **state**: Whether the project is "open" or "closed".
+#' - **creator**: The user who created the project.
+#' - **created_at**: When it was created.
+#' - **updated_at**: When it was last updated.
+#' - **html_url**: The URL to view the project.
+#'
+#' @examples
+#' \dontrun{
+#'   create_project(
+#'     name = "Repo project",
+#'     body = "This is a repository's project",
+#'     repo = "ChadGoymer/test-githapi")
+#'
+#'   create_project(
+#'     name = "User project",
+#'     body = "This is a user's project")
+#' }
 #'
 #' @export
 #'
-gh_project <- function(
-  project,
-  token = gh_token(),
-  api   = getOption("github.api"),
-  ...)
-{
-  assert(is_scalar_integerish(project) && isTRUE(project > 0))
-  assert(is_sha(token))
-  assert(is_url(api))
-
-  # NOTE: Projects is currently in beta, so requires preview accept header
-  gh_get(
-    gh_url("projects", project, api = api),
-    accept = "application/vnd.github.inertia-preview+json", token = token, ...)
-}
-
-#  FUNCTION: gh_projects ----------------------------------------------------------------------
-#
-#' List organisation or repository projects
-#'
-#' <https://developer.github.com/v3/projects/#list-repository-projects>
-#' <https://developer.github.com/v3/projects/#list-organization-projects>
-#'
-#' @param repo (string) The repository specified in the format: `owner/repo`.
-#' @param org (string) The name of the organization.
-#' @param state (string, optional) Indicates the state of the projects to return. Can be either open,
-#'   closed, or all. Default: open
-#' @param n_max (integer, optional) Maximum number to return. Default: 1000.
-#' @param token (string, optional) The personal access token for GitHub authorisation. Default:
-#'   value stored in the environment variable `GITHUB_TOKEN` or `GITHUB_PAT`.
-#' @param api (string, optional) The URL of GitHub's API. Default: the value stored in the
-#'   environment variable `GITHUB_API` or `https://api.github.com`.
-#' @param ... Parameters passed to [gh_page()].
-#'
-#' @return A tibble describing the projects (see GitHub's API documentation for details).
-#'
-#' @export
-#'
-gh_projects <- function(
+create_project <- function(
+  name,
+  body,
   repo,
   org,
-  state = NULL,
-  n_max = 1000L,
-  token = gh_token(),
-  api   = getOption("github.api"),
   ...)
 {
-  assert(is_null(state) || is_scalar_character(state))
-  assert(is_scalar_integerish(n_max) && isTRUE(n_max > 0))
-  assert(is_sha(token))
-  assert(is_url(api))
+  assert(is_scalar_character(name), "'name' must be a string:\n  ", name)
+  assert(is_scalar_character(body), "'body' must be a string:\n  ", body)
 
-  if (!missing(repo) && !missing(org))
-    error("Must specify either repo or org, not both!")
+  payload <- list(name = name, body = body)
 
-  if (!missing(repo)) {
-    assert(is_repo(repo))
-    url <- gh_url("repos", repo, "projects", state = state, api = api)
-  } else if (!missing(org)) {
-    assert(is_scalar_character(org))
-    url <- gh_url("orgs", org, "projects", state = state, api = api)
-  } else {
-    error("Must specify either repo or org!")
+  if (!missing(repo))
+  {
+    assert(is_repo(repo), "'repo' must be a string in the format 'owner/repo':\n  ", repo)
+    url <- gh_url("repos", repo, "projects")
+  }
+  else if (!missing(org))
+  {
+    assert(is_scalar_character(org), "'org' must be a string:\n  ", org)
+    url <- gh_url("orgs", org, "projects")
+  }
+  else
+  {
+    url <- gh_url("user/projects")
   }
 
-  # NOTE: Projects is currently in beta, so requires preview accept header
-  projects <- gh_page(
-    url, accept = "application/vnd.github.inertia-preview+json",
-    n_max = n_max, token = token, ...)
+  info("Posting project '", name, "'")
+  project_lst <- gh_request(
+    url     = url,
+    type    = "POST",
+    payload = payload,
+    accept  = "application/vnd.github.inertia-preview+json",
+    ...)
 
-  bind_fields(projects, list(
-    id            = c("id",               as = "integer"),
-    number        = c("number",           as = "integer"),
-    name          = c("name",             as = "character"),
-    body          = c("body",             as = "character"),
-    state         = c("state",            as = "character"),
-    creator_login = c("creator", "login", as = "character"),
-    created_at    = c("created_at",       as = "datetime"),
-    updated_at    = c("updated_at",       as = "datetime"),
-    url           = c("url",              as = "character")))
-}
+  info("Transforming results", level = 4)
+  project_tbl <- select_properties(project_lst, properties$project)
 
-#  FUNCTION: gh_column ------------------------------------------------------------------------
-#
-#' Get a project column
-#'
-#' <https://developer.github.com/v3/projects/columns/#get-a-project-column>
-#'
-#' @param column (integer) The ID of the column in GitHub.
-#' @param token (string, optional) The personal access token for GitHub authorisation. Default:
-#'   value stored in the environment variable `GITHUB_TOKEN` or `GITHUB_PAT`.
-#' @param api (string, optional) The URL of GitHub's API. Default: the value stored in the
-#'   environment variable `GITHUB_API` or `https://api.github.com`.
-#' @param ... Parameters passed to [gh_get()].
-#'
-#' @return A list describing the column (see GitHub's API documentation for details).
-#'
-#' @export
-#'
-gh_column <- function(
-  column,
-  token = gh_token(),
-  api   = getOption("github.api"),
-  ...)
-{
-  assert(is_scalar_integerish(column) && isTRUE(column > 0))
-  assert(is_sha(token))
-  assert(is_url(api))
+  project_gh <- structure(
+    project_tbl,
+    class   = c("github", class(project_tbl)),
+    url     = attr(project_lst, "url"),
+    request = attr(project_lst, "request"),
+    status  = attr(project_lst, "status"),
+    header  = attr(project_lst, "header"))
 
-  # NOTE: Projects is currently in beta, so requires preview accept header
-  gh_get(
-    gh_url("projects/columns", column, api = api),
-    accept = "application/vnd.github.inertia-preview+json", token = token, ...)
-}
-
-#  FUNCTION: gh_columns -----------------------------------------------------------------------
-#
-#' List project columns
-#'
-#' <https://developer.github.com/v3/projects/columns/#list-project-columns>
-#'
-#' @param project (integer) The ID of the project in GitHub.
-#' @param n_max (integer, optional) Maximum number to return. Default: 1000.
-#' @param token (string, optional) The personal access token for GitHub authorisation. Default:
-#'   value stored in the environment variable `GITHUB_TOKEN` or `GITHUB_PAT`.
-#' @param api (string, optional) The URL of GitHub's API. Default: the value stored in the
-#'   environment variable `GITHUB_API` or `https://api.github.com`.
-#' @param ... Parameters passed to [gh_page()].
-#'
-#' @return A tibble describing the columns (see GitHub's API documentation for details).
-#'
-#' @export
-#'
-gh_columns <- function(
-  project,
-  n_max = 1000L,
-  token = gh_token(),
-  api   = getOption("github.api"),
-  ...)
-{
-  assert(is_scalar_integerish(project) && isTRUE(project > 0))
-  assert(is_scalar_integerish(n_max) && isTRUE(n_max > 0))
-  assert(is_sha(token))
-  assert(is_url(api))
-
-  # NOTE: Projects is currently in beta, so requires preview accept header
-  columns <- gh_page(
-    gh_url("projects", project, "columns", api = api),
-    accept = "application/vnd.github.inertia-preview+json", n_max = n_max, token = token, ...)
-
-  bind_fields(columns, list(
-    id         = c("id",         as = "integer"),
-    name       = c("name",       as = "character"),
-    created_at = c("created_at", as = "datetime"),
-    updated_at = c("updated_at", as = "datetime"),
-    url        = c("url",        as = "character")))
-}
-
-#  FUNCTION: gh_card --------------------------------------------------------------------------
-#
-#' Get a project card
-#'
-#' <https://developer.github.com/v3/projects/cards/#get-a-project-card>
-#'
-#' @param card (integer) The ID of the card in GitHub.
-#' @param token (string, optional) The personal access token for GitHub authorisation. Default:
-#'   value stored in the environment variable `GITHUB_TOKEN` or `GITHUB_PAT`.
-#' @param api (string, optional) The URL of GitHub's API. Default: the value stored in the
-#'   environment variable `GITHUB_API` or `https://api.github.com`.
-#' @param ... Parameters passed to [gh_get()].
-#'
-#' @return A list describing the card (see GitHub's API documentation for details).
-#'
-#' @export
-#'
-gh_card <- function(
-  card,
-  token = gh_token(),
-  api   = getOption("github.api"),
-  ...)
-{
-  assert(is_scalar_integerish(card) && isTRUE(card > 0))
-  assert(is_sha(token))
-  assert(is_url(api))
-
-  # NOTE: Projects is currently in beta, so requires preview accept header
-  gh_get(
-    gh_url("projects/columns/cards", card, api = api),
-    accept = "application/vnd.github.inertia-preview+json", token = token, ...)
-}
-
-#  FUNCTION: gh_cards -------------------------------------------------------------------------
-#
-#' List project cards
-#'
-#' <https://developer.github.com/v3/projects/cards/#list-project-cards>
-#'
-#' @param column (integer) The ID of the column in GitHub.
-#' @param n_max (integer, optional) Maximum number to return. Default: 1000.
-#' @param token (string, optional) The personal access token for GitHub authorisation. Default:
-#'   value stored in the environment variable `GITHUB_TOKEN` or `GITHUB_PAT`.
-#' @param api (string, optional) The URL of GitHub's API. Default: the value stored in the
-#'   environment variable `GITHUB_API` or `https://api.github.com`.
-#' @param ... Parameters passed to [gh_page()].
-#'
-#' @return A tibble describing the cards (see GitHub's API documentation for details).
-#'
-#' @export
-#'
-gh_cards <- function(
-  column,
-  n_max = 1000L,
-  token = gh_token(),
-  api   = getOption("github.api"),
-  ...)
-{
-  assert(is_scalar_integerish(column) && isTRUE(column > 0))
-  assert(is_scalar_integerish(n_max) && isTRUE(n_max > 0))
-  assert(is_sha(token))
-  assert(is_url(api))
-
-  # NOTE: Projects is currently in beta, so requires preview accept header
-  cards <- gh_page(
-    gh_url("projects/columns", column, "cards", api = api),
-    accept = "application/vnd.github.inertia-preview+json",
-    n_max = n_max, token = token, ...)
-
-  bind_fields(cards, list(
-    id            = c("id",               as = "integer"),
-    creator_login = c("creator", "login", as = "character"),
-    created_at    = c("created_at",       as = "datetime"),
-    updated_at    = c("updated_at",       as = "datetime"),
-    content_url   = c("content_url",      as = "character"),
-    url           = c("url",              as = "character")))
+  info("Done", level = 7)
+  project_gh
 }
--- tests/testthat/test-projects.R
+++ tests/testthat/test-projects.R
@@ -1,88 +1,96 @@
 context("projects api")
 
-#  FUNCTION: gh_project -----------------------------------------------------------------------
-test_that("gh_project returns a list describing the project", {
-  project <- gh_project(747228)
-  expect_is(project, "list")
-  expect_named(
-    project,
-    c("owner_url", "url", "html_url", "columns_url", "id", "node_id", "name", "body",
-      "number", "state", "creator", "created_at", "updated_at"))
-  expect_identical(project$name, "Prioritisation")
-})
 
-#  FUNCTION: gh_projects ----------------------------------------------------------------------
-test_that("gh_projects returns a tibble describing the projects", {
-  projects <- gh_projects("ChadGoymer/githapi")
-  expect_is(projects, "tbl")
+# TEST: create_project ------------------------------------------------------------------------
 
+test_that("create_projects creates a project and returns its properties", {
+
+  repo_project <- create_project(
+    name = "Repo project",
+    body = "This is a repo project",
+    repo = "ChadGoymer/test-githapi")
+
+  expect_is(repo_project, "list")
+  expect_identical(attr(repo_project, "status"), 201L)
   expect_identical(
-    sapply(projects, function(field) class(field)[[1]]),
-    c(id            = "integer",
-      number        = "integer",
-      name          = "character",
-      body          = "character",
-      state         = "character",
-      creator_login = "character",
-      created_at    = "POSIXct",
-      updated_at    = "POSIXct",
-      url           = "character"))
-
-  expect_true("Prioritisation" %in% projects$name)
-})
-
-#  FUNCTION: gh_column ------------------------------------------------------------------------
-test_that("gh_column returns a list describing the column", {
-  column <- gh_column(1310204)
-  expect_is(column, "list")
-  expect_named(
-    column,
-    c("url", "project_url", "cards_url", "id", "node_id", "name", "created_at", "updated_at"))
-  expect_identical(column$name, "Triage")
-})
-
-#  FUNCTION: gh_columns -----------------------------------------------------------------------
-test_that("gh_columns returns a tibble describing the columns", {
-  columns <- gh_columns(747228)
-  expect_is(columns, "tbl")
-
-  expect_identical(
-    sapply(columns, function(field) class(field)[[1]]),
+    map_chr(repo_project, ~ class(.)[[1]]),
     c(id         = "integer",
+      number     = "integer",
       name       = "character",
+      body       = "character",
+      state      = "character",
+      permission = "character",
+      private    = "logical",
+      creator    = "character",
       created_at = "POSIXct",
       updated_at = "POSIXct",
-      url        = "character"))
+      html_url   = "character"))
 
-  expect_true(all(c("Triage", "Sometime", "Soon", "Now", "Done") %in% columns$name))
+  expect_identical(repo_project$name, "Repo project")
+
+  user_project <- create_project(
+    name = "User project",
+    body = "This is a user project")
+
+  expect_is(user_project, "list")
+  expect_identical(attr(user_project, "status"), 201L)
+  expect_identical(
+    map_chr(user_project, ~ class(.)[[1]]),
+    c(id         = "integer",
+      number     = "integer",
+      name       = "character",
+      body       = "character",
+      state      = "character",
+      permission = "character",
+      private    = "logical",
+      creator    = "character",
+      created_at = "POSIXct",
+      updated_at = "POSIXct",
+      html_url   = "character"))
+
+  expect_identical(user_project$name, "User project")
+
+  org_project <- create_project(
+    name = "Organisation project",
+    body = "This is an organisation project",
+    org  = "HairyCoos")
+
+  expect_is(org_project, "list")
+  expect_identical(attr(org_project, "status"), 201L)
+  expect_identical(
+    map_chr(org_project, ~ class(.)[[1]]),
+    c(id         = "integer",
+      number     = "integer",
+      name       = "character",
+      body       = "character",
+      state      = "character",
+      permission = "character",
+      private    = "logical",
+      creator    = "character",
+      created_at = "POSIXct",
+      updated_at = "POSIXct",
+      html_url   = "character"))
+
+  expect_identical(org_project$name, "Organisation project")
+
 })
 
-#  FUNCTION: gh_card --------------------------------------------------------------------------
-test_that("gh_card returns a list describing the card", {
-  card <- gh_card(4211067)
-  expect_is(card, "list")
-  expect_named(
-    card,
-    c("url", "project_url", "id", "node_id", "note", "archived", "creator", "created_at",
-      "updated_at", "column_url", "content_url"))
-  expect_identical(
-    card$content_url,
-    "https://api.github.com/repos/ChadGoymer/githapi/issues/16")
-})
+test_that("create_project throws an error if invalid arguments are supplied", {
 
-#  FUNCTION: gh_cards -------------------------------------------------------------------------
-test_that("gh_cards returns a tibble describing the cards", {
-  cards <- gh_cards(1310204)
-  expect_is(cards, "tbl")
+  expect_error(
+    create_project(name = 1, body = "This is an invalid project"),
+    "'name' must be a string")
 
-  expect_identical(
-    sapply(cards, function(field) class(field)[[1]]),
-    c(id            = "integer",
-      creator_login = "character",
-      created_at    = "POSIXct",
-      updated_at    = "POSIXct",
-      content_url   = "character",
-      url           = "character"))
+  expect_error(
+    create_project(name = "invalid project", body = 1),
+    "'body' must be a string")
 
-  expect_true("ChadGoymer" %in% cards$creator_login)
+  expect_error(
+    create_project(name = "invalid project", body = "This is an invalid project", repo = 1),
+    "'repo' must be a string in the format 'owner/repo'")
+
+  expect_error(
+    create_project(name = "invalid project", body = "This is an invalid project", org = 1),
+    "'org' must be a string")
+
 })
