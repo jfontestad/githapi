d7512d76e794420cdab6b43357b63525d4ac72b0
Chad <Chad.Goymer@gmail.com>
2019-12-11 09:28:17 +0000

    updated utilities tests to work on Travis

--- tests/testthat/test-utilities.R
+++ tests/testthat/test-utilities.R
@@ -36,11 +36,15 @@ test_that("is_repo returns TRUE with a valid repo and FALSE otherwise", {
 
 test_that("as.datetime converts a datetime string from UTC to local timezone", {
 
-  bst <- as.character(as.datetime("2019-08-01T10:00:00Z"))
-  expect_identical(bst, "2019-08-01 11:00:00")
+  summer <- as.character(as.datetime("2019-08-01T10:00:00Z"))
+  local_summer <- as.POSIXct("2019-08-01 10:00:00", tz = "UTC") %>% format(tz = "")
 
-  gmt <- as.character(as.datetime("2019-11-01T10:00:00Z"))
-  expect_identical(gmt, "2019-11-01 10:00:00")
+  expect_identical(summer, local_summer)
+
+  winter <- as.character(as.datetime("2019-11-01T10:00:00Z"))
+  local_winter <- as.POSIXct("2019-11-01 10:00:00", tz = "UTC") %>% format(tz = "")
+
+  expect_identical(winter, local_winter)
 
 })
 
@@ -65,51 +69,45 @@ test_that("property_names returns appropriate names", {
 test_that("select_properties returns a list of converted properties", {
 
   entity <- list(
-    version      = "182.0.1.9757",
-    release_type = "Test",
-    release_date = "2019-01-01T00:00:00Z",
-    console_exe  = ".\\TycheConsole.exe",
-    tester_exe   = ".\\TycheTester.exe",
-    breaking     = FALSE,
-    is_hidden    = FALSE,
-    created_by   = list(first_name = "Bob", last_name = "Smith"),
-    created_at   = "2019-01-05T07:54:41Z",
-    updated_by   = list(first_name = "Jane"),
-    updated_at   = "2019-02-01T07:13:41Z")
+    id                      = 3534197,
+    number                  = 1,
+    name                    = "Work in Progress",
+    body                    = "All my work in progress",
+    state                   = "open",
+    organization_permission = NA,
+    private                 = NA,
+    creator                 = list(login = "ChadGoymer"),
+    created_at              = "2019-11-16T07:20:09Z",
+    updated_at              = "2019-12-10T19:07:53Z",
+    html_url                = "https://github.com/users/ChadGoymer/projects/1")
 
   properties <- list(
-    version          = c("version",                  as = "factor"),
-    release_type     = c("release_type",             as = "factor"),
-    release_date     = c("release_date",             as = "datetime"),
-    location         = c("location",                 as = "character"),
-    console_exe      = c("console_exe",              as = "character"),
-    tester_exe       = c("tester_exe",               as = "character"),
-    breaking         = c("breaking",                 as = "logical"),
-    is_hidden        = c("is_hidden",                as = "logical"),
-    created_by_first = c("created_by", "first_name", as = "character"),
-    created_by_last  = c("created_by", "last_name",  as = "character"),
-    created_at       = c("created_at",               as = "datetime"),
-    updated_by_first = c("updated_by", "first_name", as = "character"),
-    updated_by_last  = c("updated_by", "last_name",  as = "character"),
-    updated_at       = c("updated_at",               as = "datetime"))
+    id         = c("id",                      as = "integer"),
+    number     = c("number",                  as = "integer"),
+    name       = c("name",                    as = "character"),
+    body       = c("body",                    as = "character"),
+    state      = c("state",                   as = "character"),
+    permission = c("organization_permission", as = "character"),
+    private    = c("private",                 as = "logical"),
+    creator    = c("creator", "login",        as = "character"),
+    created_at = c("created_at",              as = "datetime"),
+    updated_at = c("updated_at",              as = "datetime"),
+    html_url   = c("html_url",                as = "character"))
 
   selected_properties <- select_properties(entity, properties)
 
   expected_result <- list(
-    version          = as.factor("182.0.1.9757"),
-    release_type     = as.factor("Test"),
-    release_date     = as.POSIXct("2019-01-01 00:00:00"),
-    location         = NA_character_,
-    console_exe      = ".\\TycheConsole.exe",
-    tester_exe       = ".\\TycheTester.exe",
-    breaking         = FALSE,
-    is_hidden        = FALSE,
-    created_by_first = "Bob",
-    created_by_last  = "Smith",
-    created_at       = as.POSIXct("2019-01-05 07:54:41"),
-    updated_by_first = "Jane",
-    updated_by_last  = NA_character_,
-    updated_at       = as.POSIXct("2019-02-01 07:13:41"))
+    id         = as.integer(3534197),
+    number     = as.integer(1),
+    name       = "Work in Progress",
+    body       = "All my work in progress",
+    state      = "open",
+    permission = as.character(NA),
+    private    = NA,
+    creator    = "ChadGoymer",
+    created_at = as.POSIXct("2019-11-16 07:20:09", tz = "UTC") %>% format(tz = "") %>% as.POSIXct(),
+    updated_at = as.POSIXct("2019-12-10 19:07:53", tz = "UTC") %>% format(tz = "") %>% as.POSIXct(),
+    html_url   = "https://github.com/users/ChadGoymer/projects/1")
 
   expect_identical(selected_properties, expected_result)
 
@@ -118,38 +116,32 @@ test_that("select_properties returns a list of converted properties", {
 test_that("select_properties returns a list of empty vectors for an empty entity", {
 
   properties <- list(
-    version          = c("version",                  as = "factor"),
-    release_type     = c("release_type",             as = "factor"),
-    release_date     = c("release_date",             as = "datetime"),
-    location         = c("location",                 as = "character"),
-    console_exe      = c("console_exe",              as = "character"),
-    tester_exe       = c("tester_exe",               as = "character"),
-    breaking         = c("breaking",                 as = "logical"),
-    is_hidden        = c("is_hidden",                as = "logical"),
-    created_by_first = c("created_by", "first_name", as = "character"),
-    created_by_last  = c("created_by", "last_name",  as = "character"),
-    created_at       = c("created_at",               as = "datetime"),
-    updated_by_first = c("updated_by", "first_name", as = "character"),
-    updated_by_last  = c("updated_by", "last_name",  as = "character"),
-    updated_at       = c("updated_at",               as = "datetime"))
+    id         = c("id",                      as = "integer"),
+    number     = c("number",                  as = "integer"),
+    name       = c("name",                    as = "character"),
+    body       = c("body",                    as = "character"),
+    state      = c("state",                   as = "character"),
+    permission = c("organization_permission", as = "character"),
+    private    = c("private",                 as = "logical"),
+    creator    = c("creator", "login",        as = "character"),
+    created_at = c("created_at",              as = "datetime"),
+    updated_at = c("updated_at",              as = "datetime"),
+    html_url   = c("html_url",                as = "character"))
 
   empty_result <- select_properties(list(), properties)
 
   expected_empty_result <- list(
-    version          = as.factor(character()),
-    release_type     = as.factor(character()),
-    release_date     = as.POSIXct(character()),
-    location         = character(),
-    console_exe      = character(),
-    tester_exe       = character(),
-    breaking         = logical(),
-    is_hidden        = logical(),
-    created_by_first = character(),
-    created_by_last  = character(),
-    created_at       = as.POSIXct(character()),
-    updated_by_first = character(),
-    updated_by_last  = character(),
-    updated_at       = as.POSIXct(character()))
+    id         = integer(),
+    number     = integer(),
+    name       = character(),
+    body       = character(),
+    state      = character(),
+    permission = character(),
+    private    = logical(),
+    creator    = character(),
+    created_at = as.POSIXct(character()),
+    updated_at = as.POSIXct(character()),
+    html_url   = character())
 
   expect_identical(empty_result, expected_empty_result)
 
@@ -162,103 +154,92 @@ test_that("bind_properties extracts properties from entities and binds them into
 
   entities <- list(
     list(
-      version      = "182.0.1.9757",
-      release_type = "Test",
-      release_date = "2019-01-01T00:00:00Z",
-      console_exe  = ".\\TycheConsole.exe",
-      tester_exe   = ".\\TycheTester.exe",
-      breaking     = FALSE,
-      is_hidden    = FALSE,
-      created_by   = list(first_name = "Bob", last_name = "Smith"),
-      created_at   = "2019-01-05T07:54:41Z",
-      updated_by   = list(first_name = "Jane"),
-      updated_at   = "2019-02-01T07:13:41Z"),
+      id                      = 3534197,
+      number                  = 1,
+      name                    = "Work in Progress",
+      body                    = "All my work in progress",
+      state                   = "open",
+      organization_permission = NA,
+      private                 = NA,
+      creator                 = list(login = "ChadGoymer"),
+      created_at              = "2019-11-16T07:20:09Z",
+      updated_at              = "2019-12-10T19:07:53Z",
+      html_url                = "https://github.com/users/ChadGoymer/projects/1"),
     list(
-      version      = "182.0.2.4242",
-      release_type = "Production",
-      release_date = "2019-04-01T00:00:00Z",
-      location     = "C:\\Tyche\\182.0.2.4242",
-      console_exe  = ".\\TycheConsole.exe",
-      tester_exe   = NULL,
-      breaking     = TRUE,
-      is_hidden    = FALSE,
-      created_by   = list(first_name = "Jane", last_name = "Jones"),
-      created_at   = "2019-04-05T07:54:41Z",
-      updated_by   = NULL,
-      updated_at   = "2019-05-01T07:13:41Z"))
+      id                      = 747228,
+      number                  = 1,
+      name                    = "Prioritisation",
+      body                    = "All tasks in priority order",
+      state                   = "open",
+      organization_permission = NA,
+      private                 = NA,
+      creator                 = list(login = "ChadGoymer"),
+      created_at              = "2017-07-11T11:24:23Z",
+      updated_at              = "2019-12-07T12:53:24Z",
+      html_url                = "https://github.com/ChadGoymer/githapi/projects/1")
+  )
 
   properties <- list(
-    version          = c("version",                  as = "factor"),
-    release_type     = c("release_type",             as = "factor"),
-    release_date     = c("release_date",             as = "datetime"),
-    location         = c("location",                 as = "character"),
-    console_exe      = c("console_exe",              as = "character"),
-    tester_exe       = c("tester_exe",               as = "character"),
-    breaking         = c("breaking",                 as = "logical"),
-    is_hidden        = c("is_hidden",                as = "logical"),
-    created_by_first = c("created_by", "first_name", as = "character"),
-    created_by_last  = c("created_by", "last_name",  as = "character"),
-    created_at       = c("created_at",               as = "datetime"),
-    updated_by_first = c("updated_by", "first_name", as = "character"),
-    updated_by_last  = c("updated_by", "last_name",  as = "character"),
-    updated_at       = c("updated_at",               as = "datetime"))
+    id         = c("id",                      as = "integer"),
+    number     = c("number",                  as = "integer"),
+    name       = c("name",                    as = "character"),
+    body       = c("body",                    as = "character"),
+    state      = c("state",                   as = "character"),
+    permission = c("organization_permission", as = "character"),
+    private    = c("private",                 as = "logical"),
+    creator    = c("creator", "login",        as = "character"),
+    created_at = c("created_at",              as = "datetime"),
+    updated_at = c("updated_at",              as = "datetime"),
+    html_url   = c("html_url",                as = "character"))
 
   result <- bind_properties(entities, properties)
 
   expected_result <- tibble(
-    version          = as.factor(c("182.0.1.9757", "182.0.2.4242")),
-    release_type     = as.factor(c("Test", "Production")),
-    release_date     = as.POSIXct(c("2019-01-01 00:00:00", "2019-04-01 01:00:00")),
-    location         = c(NA_character_, "C:\\Tyche\\182.0.2.4242"),
-    console_exe      = c(".\\TycheConsole.exe", ".\\TycheConsole.exe"),
-    tester_exe       = c(".\\TycheTester.exe", NA_character_),
-    breaking         = c(FALSE, TRUE),
-    is_hidden        = c(FALSE, FALSE),
-    created_by_first = c("Bob", "Jane"),
-    created_by_last  = c("Smith", "Jones"),
-    created_at       = as.POSIXct(c("2019-01-05 07:54:41", "2019-04-05 08:54:41")),
-    updated_by_first = c("Jane", NA_character_),
-    updated_by_last  = c(NA_character_, NA_character_),
-    updated_at       = as.POSIXct(c("2019-02-01 07:13:41", "2019-05-01 08:13:41")))
+    id         = as.integer(c(3534197, 747228)),
+    number     = as.integer(c(1, 1)),
+    name       = c("Work in Progress", "Prioritisation"),
+    body       = c("All my work in progress", "All tasks in priority order"),
+    state      = c("open", "open"),
+    permission = as.character(c(NA, NA)),
+    private    = c(NA, NA),
+    creator    = c("ChadGoymer", "ChadGoymer"),
+    created_at = as.POSIXct(c("2019-11-16 07:20:09", "2017-07-11 11:24:23"), tz = "UTC") %>% format(tz = "") %>% as.POSIXct(),
+    updated_at = as.POSIXct(c("2019-12-10 19:07:53", "2019-12-07 12:53:24"), tz = "UTC") %>% format(tz = "") %>% as.POSIXct(),
+    html_url   = c("https://github.com/users/ChadGoymer/projects/1", "https://github.com/ChadGoymer/githapi/projects/1"))
 
   expect_identical(result, expected_result)
+
 })
 
 test_that("bind_properties returns an empty data.frame for an empty list of entities", {
 
   properties <- list(
-    version          = c("version",                  as = "factor"),
-    release_type     = c("release_type",             as = "factor"),
-    release_date     = c("release_date",             as = "datetime"),
-    location         = c("location",                 as = "character"),
-    console_exe      = c("console_exe",              as = "character"),
-    tester_exe       = c("tester_exe",               as = "character"),
-    breaking         = c("breaking",                 as = "logical"),
-    is_hidden        = c("is_hidden",                as = "logical"),
-    created_by_first = c("created_by", "first_name", as = "character"),
-    created_by_last  = c("created_by", "last_name",  as = "character"),
-    created_at       = c("created_at",               as = "datetime"),
-    updated_by_first = c("updated_by", "first_name", as = "character"),
-    updated_by_last  = c("updated_by", "last_name",  as = "character"),
-    updated_at       = c("updated_at",               as = "datetime"))
+    id         = c("id",                      as = "integer"),
+    number     = c("number",                  as = "integer"),
+    name       = c("name",                    as = "character"),
+    body       = c("body",                    as = "character"),
+    state      = c("state",                   as = "character"),
+    permission = c("organization_permission", as = "character"),
+    private    = c("private",                 as = "logical"),
+    creator    = c("creator", "login",        as = "character"),
+    created_at = c("created_at",              as = "datetime"),
+    updated_at = c("updated_at",              as = "datetime"),
+    html_url   = c("html_url",                as = "character"))
 
   empty_result <- bind_properties(list(), properties)
 
   expected_empty_result <- tibble(
-    version          = as.factor(character()),
-    release_type     = as.factor(character()),
-    release_date     = as.POSIXct(character()),
-    location         = character(),
-    console_exe      = character(),
-    tester_exe       = character(),
-    breaking         = logical(),
-    is_hidden        = logical(),
-    created_by_first = character(),
-    created_by_last  = character(),
-    created_at       = as.POSIXct(character()),
-    updated_by_first = character(),
-    updated_by_last  = character(),
-    updated_at       = as.POSIXct(character()))
+    id         = integer(),
+    number     = integer(),
+    name       = character(),
+    body       = character(),
+    state      = character(),
+    permission = character(),
+    private    = logical(),
+    creator    = character(),
+    created_at = as.POSIXct(character()),
+    updated_at = as.POSIXct(character()),
+    html_url   = character())
 
   expect_identical(empty_result, expected_empty_result)
 
