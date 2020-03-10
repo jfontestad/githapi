c588780a6f5fa3d84975a75be89272fbc072da5a
Chad <Chad.Goymer@gmail.com>
2020-03-10 08:20:04 +0000

    Updated package documentation

--- R/githapi-deprecated.R
+++ R/githapi-deprecated.R
@@ -49,8 +49,8 @@
 #' - [gh_issues()]: replace with [view_issues()]
 #' - [gh_user_issues()]: replace with [view_issues()]
 #' - [gh_assignees()]: is being removed
-#' - [gh_issue_comments()]: replace with [view_comments()]
-#' - [gh_issue_comment()]: is being removed
+#' - [gh_issue_comment()]: replace with `view_comment()`
+#' - [gh_issue_comments()]: replace with `view_comments()`
 #' - [gh_event()]: is being removed
 #' - [gh_events()]: is being removed
 #'
--- ci/deploy-package.R
+++ ci/deploy-package.R
@@ -1,7 +1,7 @@
 
+pkgdown::build_site()
+
 devtools::check_rhub(env_vars = c(`_R_CHECK_FORCE_SUGGESTS_` = "false"))
 devtools::check_win_devel()
-
-pkgdown::build_site()
 devtools::release()
 
